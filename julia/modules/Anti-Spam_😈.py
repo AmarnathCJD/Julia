bf89d746aace15fb3454c40863360991df44558b
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2020-11-30 13:03:16 +0530

    Update Anti-Spam_😈.py

--- julia/modules/Anti-Spam_😈.py
+++ julia/modules/Anti-Spam_😈.py
@@ -18,7 +18,7 @@ client = MongoClient(MONGO_DB_URI)
 db = client["missjuliarobot"]
 approved_users = db.approve
 spammers = db.spammer
-cleanservices = dbb.cleanservice
+cleanservices = db.cleanservice
 
 CMD_STARTERS = '/'
 profanity.load_censor_words_from_file('./profanity_wordlist.txt')
