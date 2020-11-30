84dac10fa022240a5f6136627c9ba8581c413340
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2020-11-30 13:16:05 +0530

    Update Anti-Spam_😈.py

--- julia/modules/Anti-Spam_😈.py
+++ julia/modules/Anti-Spam_😈.py
@@ -301,6 +301,7 @@ async def cleanservice(event):
                     return
         await event.reply(
                     "Service message cleaning isn't turned on for this chat.")       
+        return
     if not input == "on" or input == "off":
         await event.reply("I only understand by on or off")
         return
