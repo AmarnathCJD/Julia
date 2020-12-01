7dbb4bb22958c6149d6482105ad13939f090a2b6
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2020-12-01 22:31:06 +0530

    Update Anti-Spam_😈.py

--- julia/modules/Anti-Spam_😈.py
+++ julia/modules/Anti-Spam_😈.py
@@ -452,6 +452,7 @@ __help__ = """
  - /listcleanbluetext: list currently whitelisted commands
  - /profanity on/off: filters all explict/abusive words sent by non admins also filters explicit/porn images
  - /cleanservice on/off: cleans all service messages from telegram
+ - /globalmode: let users only speak in english in your group (automatically deletes messages in other languages)
 """
 
 CMD_HELP.update({
