a0fbc31891c7a9f75b54bab7d22988c45935f8ed
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2020-12-02 22:30:13 +0530

    Update Anti-Virus_🐞.py

--- julia/modules/Anti-Virus_🐞.py
+++ julia/modules/Anti-Virus_🐞.py
@@ -59,7 +59,7 @@ async def virusscan(event):
     chat = "@VS_Robot"
     async with ubot.conversation(chat) as conv:
         try:
-            file = await ubot.download_file(c.media)
+            file = await event.client.download_file(c.media)
             response = conv.wait_event(events.NewMessage(incoming=True, from_users=299969270))
             await ubot.send_file(chat, file)
             response = await response
