85470b30a7cbcd8bab885db8da1868387cef5634
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2020-12-02 16:03:49 +0530

    Update Anti-Spam_😈.py

--- julia/modules/Anti-Spam_😈.py
+++ julia/modules/Anti-Spam_😈.py
@@ -13,6 +13,7 @@ from julia import *
 import better_profanity
 from better_profanity import profanity
 from langdetect import detect_langs
+from langdetect import DetectorFactory
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
@@ -24,7 +25,7 @@ globalchat = db.globchat
 
 CMD_STARTERS = '/'
 profanity.load_censor_words_from_file('./profanity_wordlist.txt')
-
+DetectorFactory.seed = 0
 
 async def can_change_info(message):
     result = await tbot(
@@ -420,7 +421,7 @@ async def del_profanity(event):
             if event.chat_id == c['id']:
                 a = detect_langs(msg)
                 for ele in a:
-                 if str(ele).split(':')[0] == 'en':
+                 if str(ele).split(':')[0] != 'en':
                     await event.delete()
                     if sender.username is None:
                         st = sender.first_name
