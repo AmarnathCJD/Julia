901f0824b6282c46d5466aa327d9e040451fcc4d
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2020-12-03 18:21:14 +0530

    Update Anti-Virus_🐞.py

--- julia/modules/Anti-Virus_🐞.py
+++ julia/modules/Anti-Virus_🐞.py
@@ -38,7 +38,7 @@ async def is_register_admin(chat, user):
 async def virusscan(event):
     if event.fwd_from:
         return
-    #sender_id = event.sender_id
+    #sender_id = event.message
     approved_userss = approved_users.find({})
     for ch in approved_userss:
         iid = ch["id"]
@@ -68,7 +68,7 @@ async def virusscan(event):
       response = await y.wait_event(events.MessageEdited(from_users=o.id))
       response = await y.wait_event(events.MessageEdited(from_users=o.id))
       response = await y.wait_event(events.MessageEdited(from_users=o.id))
-      await event.reply(event.chat_id, response.message)
+      await tbot.send_message(event.chat_id, response.message, reply_to=sender_id)
      except Exception as e:
       os.remove(virus)
       await event.reply("Some error occurred.")
