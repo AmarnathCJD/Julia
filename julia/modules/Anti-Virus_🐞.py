19fcc335ea054cfa1b363c2dc3e7b2a7e6d8f8d7
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2020-12-07 11:00:57 +0530

    Update Anti-Virus_🐞.py

--- julia/modules/Anti-Virus_🐞.py
+++ julia/modules/Anti-Virus_🐞.py
@@ -204,7 +204,7 @@ async def virusscanner(event):
       gg= await event.reply("Scanning the file ...")
       api_response = api_instance.scan_file(c.file.name)
       print(api_response)
-      if api_response.clean_result == "True":
+      if api_response.clean_result == True:
        await gg.edit("This file is safe ✔️\nNo virus detected 🐞")
       else:
        await gg.edit("This file is Dangerous ☠️️\nVirus detected 🐞")
