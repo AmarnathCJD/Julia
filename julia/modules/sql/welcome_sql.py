1093952ed66817300791842091c628056bfc2df0
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2020-12-22 20:40:49 +0530

    Update welcome_sql.py

--- julia/modules/sql/welcome_sql.py
+++ julia/modules/sql/welcome_sql.py
@@ -37,6 +37,13 @@ def get_current_welcome_settings(chat_id):
     finally:
         SESSION.close()
 
+def get_current_goodbye_settings(chat_id):
+    try:
+        return SESSION.query(Welcome).filter(Welcome.chat_id == str(chat_id)).one()
+    except:
+        return None
+    finally:
+        SESSION.close()
 
 def add_welcome_setting(
     chat_id,
@@ -56,6 +63,23 @@ def add_welcome_setting(
     SESSION.add(adder)
     SESSION.commit()
 
+def add_goodbye_setting(
+    chat_id,
+    custom_welcome_message,
+    should_clean_welcome,
+    previous_welcome,
+    media_file_id,
+):
+    # adder = SESSION.query(Welcome).get(chat_id)
+    adder = Welcome(
+        chat_id,
+        custom_welcome_message,
+        should_clean_welcome,
+        previous_welcome,
+        media_file_id,
+    )
+    SESSION.add(adder)
+    SESSION.commit()
 
 def rm_welcome_setting(chat_id):
     rem = SESSION.query(Welcome).get(str(chat_id))
@@ -63,8 +87,19 @@ def rm_welcome_setting(chat_id):
         SESSION.delete(rem)
         SESSION.commit()
 
+def rm_goodbye_setting(chat_id):
+    rem = SESSION.query(Welcome).get(str(chat_id))
+    if rem:
+        SESSION.delete(rem)
+        SESSION.commit()
 
 def update_previous_welcome(chat_id, previous_welcome):
+    row = SESSION.query(Welcome).get(chat_id)
+    row.previous_welcome = previous_welcome
+    # commit the changes to the DB
+    SESSION.commit()
+
+def update_previous_goodbye(chat_id, previous_welcome):
     row = SESSION.query(Welcome).get(chat_id)
     row.previous_welcome = previous_welcome
     # commit the changes to the DB
