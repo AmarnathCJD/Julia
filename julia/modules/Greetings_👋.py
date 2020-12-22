9739750bb7ed3335a3a399c6b79ab3d5fa759e56
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2020-12-22 20:36:00 +0530

    Update Greetings_👋.py

--- julia/modules/Greetings_👋.py
+++ julia/modules/Greetings_👋.py
@@ -9,6 +9,13 @@ from julia.modules.sql.welcome_sql import (
     rm_welcome_setting,
     update_previous_welcome,
 )
+from julia.modules.sql.goodbye_sql import (
+    add_goodbye_setting,
+    get_current_goodbye_settings,
+    rm_goodbye_setting,
+    update_previous_goodbye,
+)
+
 
 @tbot.on(events.ChatAction())  # pylint:disable=E0602
 async def _(event):
@@ -102,6 +109,99 @@ async def _(event):
         )
     else:
         await event.reply("No welcome message found for this chat")
+
+@tbot.on(events.ChatAction())  # pylint:disable=E0602
+async def _(event):
+    cws = get_current_goodbye_settings(event.chat_id)
+    if cws:
+        # logger.info(event.stringify())
+        """user_added=False,
+        user_joined=True,
+        user_left=False,
+        user_kicked=False,"""
+        if event.user_left:
+            if cws.should_clean_goodbye:
+                try:
+                    await tbot.delete_messages(  # pylint:disable=E0602
+                        event.chat_id, cws.previous_goodbye
+                    )
+                except Exception as e:  # pylint:disable=C0103,W0703
+                    logger.warn(str(e))  # pylint:disable=E0602
+            a_user = await event.get_user()
+            chat = await event.get_chat()
+            me = await tbot.get_me()
+
+            title = chat.title if chat.title else "this chat"
+            participants = await event.client.get_participants(chat)
+            count = len(participants)
+            mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
+            first = a_user.first_name
+            last = a_user.last_name
+            if last:
+                fullname = f"{first} {last}"
+            else:
+                fullname = first
+            username = (
+                f"@{me.username}" if me.username else f"[Me](tg://user?id={me.id})"
+            )
+            userid = a_user.id
+            current_saved_goodbye_message = cws.custom_goodbye_message
+            mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
+
+            current_message = await event.reply(
+                current_saved_goodbye_message.format(
+                    mention=mention,
+                    title=title,
+                    count=count,
+                    first=first,
+                    last=last,
+                    fullname=fullname,
+                    username=username,
+                    userid=userid,
+                ),
+                file=cws.media_file_id,
+            )
+            update_previous_goodbye(event.chat_id, current_message.id)
+
+@register(pattern="^/setgoodbye")  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    msg = await event.get_reply_message()
+    if msg and msg.media:
+        tbot_api_file_id = pack_bot_file_id(msg.media)
+        add_goodbye_setting(event.chat_id, msg.message, True, 0, tbot_api_file_id)
+        await event.reply("Goodbye message saved. ")
+    else:
+        input_str = event.text.split(None, 1)
+        add_goodbye_setting(event.chat_id, input_str[1], True, 0, None)
+        await event.reply("Goodbye message saved. ")
+
+
+@register(pattern="^/cleargoodbye$")  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    cws = get_current_goodbye_settings(event.chat_id)
+    rm_goodbye_setting(event.chat_id)
+    await event.reply(
+        "Goodbye message cleared. "
+        + "The previous goodbye message was `{}`".format(cws.custom_goodbye_message)
+    )
+
+
+@register(pattern="^/checkgoodbye$")  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    cws = get_current_goodbye_settings(event.chat_id)
+    if hasattr(cws, "custom_goodbye_message"):
+        await event.reply(
+           "This chat's goodbye message is\n\n`{}`".format(cws.custom_goodbye_message)
+        )
+    else:
+        await event.reply("No goodbye message found for this chat")
+
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
