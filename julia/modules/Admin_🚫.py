4776e52bb6d2fe5dbc34343a82063cc6c30bf366
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-01-09 15:31:39 +0530

    Update Admin_🚫.py

--- julia/modules/Admin_🚫.py
+++ julia/modules/Admin_🚫.py
@@ -38,7 +38,6 @@ PP_TOO_SMOL = "The image is too small"
 PP_ERROR = "Failure while processing image"
 NO_ADMIN = "I am not an admin"
 NO_PERM = "I don't have sufficient permissions!"
-NO_SQL = "Database connections failing!"
 
 CHAT_PP_CHANGED = "Chat Picture Changed"
 CHAT_PP_ERROR = "Some issue with updating the pic," \
