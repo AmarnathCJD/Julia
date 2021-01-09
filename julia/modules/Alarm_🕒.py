8fddf128dc719a38da04bd457e3b8fc2028e4c1f
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-01-09 14:41:59 +0530

    Update Alarm_🕒.py

--- julia/modules/Alarm_🕒.py
+++ julia/modules/Alarm_🕒.py
@@ -67,9 +67,7 @@ async def tikclock(event):
      print(present)
      print (zone)
      #print(present>=ttime)
-     if not present >= ttime:
-       return       
-       break
-     await tbot.send_message(chat, f"**DING DONG**\n\n__This is an alarm set by__ {user} __for reason -__ `{reason}`")
-     alarms.delete_one({"chat": chat, "user": user, "time": time, "zone": zone, "reason": reason})
-     break
+     if ttime >= present:
+      await tbot.send_message(chat, f"**DING DONG**\n\n__This is an alarm set by__ {user} __for reason -__ `{reason}`")
+      alarms.delete_one({"chat": chat, "user": user, "time": time, "zone": zone, "reason": reason})
+      break
