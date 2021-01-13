51a744258586a4017c880f97429225adc5a0e067
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-01-14 00:31:37 +0530

    Update Federation_🔗.py

--- julia/modules/Federation_🔗.py
+++ julia/modules/Federation_🔗.py
@@ -60,7 +60,7 @@ async def _(event):
     chat = event.chat
     user = event.sender
     message = event.message
-    if event.is_private:
+    if not event.is_private:
         await event.reply(
             "Federations can only be deleted by privately messaging me.")
         return
