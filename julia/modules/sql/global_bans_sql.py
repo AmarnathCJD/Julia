efd427250adf733e93df1cec8c274194684b7720
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-24 20:42:55 +0530

    Update global_bans_sql.py

--- julia/modules/sql/global_bans_sql.py
+++ julia/modules/sql/global_bans_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from LEGEND.modules.sql import BASE, SESSION
+from julia.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
 
