9af82c521794bdbb393c9c86539cc44d18dd5b3d
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-26 20:29:15 +0530

    Add files via upload

---
+++ LEGEND/legend.py
@@ -0,0 +1,13 @@
+#maked by LEGENDX22 OR LEGENDXOP
+#Made by TEAM LEGEND
+import os
+demon = os.environ.get("DEMONS")
+tigers = os.environ.get("TIGERS")
+dragon = os.environ.get("DRAGONS")
+wolves = os.environ.get("WOLVES")
+dev = os.environ.get("DEV_USERS")
+owner = os.environ.get("OWNER_USERNAME")
+
+creator = "LEGEND X"
+ 
+team = "MY OFFICIAL TEAM IS TEAM LEGEND 🔥"
