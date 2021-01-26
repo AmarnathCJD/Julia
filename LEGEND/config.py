e29885aa562b73a6671cf09530a56386f6d6780e
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-26 21:08:37 +0530

    Update and rename sample_config.py to config.py

--- LEGEND/config.py
+++ LEGEND/config.py
@@ -15,14 +15,14 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1100231654 # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "LEGENDX22"
+    API_ID = 1822414  # integer value, dont use ""
+    API_HASH = "46f1888d3f68396bad08c92ac4d7f00a"
+    TOKEN = "1484701846:AAFBP9KkqJwhtfRgvssr4_LB-8CP9567FFQ"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1221693726 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "RoseloverX"
     SUPPORT_CHAT = 'teamishere'  #Your own group for support, do not add the @
-    JOIN_LOGGER = -1001253661229  #Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = -1001190806654  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    JOIN_LOGGER = -1001433850650  #Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = -1001433850650  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename'  # needed for any database modules
