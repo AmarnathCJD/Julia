40b25be66bf56e5ea36f788bfd3569fd78221e91
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-30 16:56:48 +0530

    Add files via upload

---
+++ julia/modules/test.py
@@ -0,0 +1,46 @@
+
+import io
+import os
+import asyncio
+ julia.events import register
+from telethon import *
+from telethon.tl import functions
+from telethon.tl.types import *
+from julia import *
+
+
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await
+             tbot(functions.channels.GetParticipantRequest(chat,
+                                                           user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await tbot.get_peer_id(user)
+        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id)
+                         )).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
+
+@register(pattern="^/test$")
+async def msg(event):
+    approved_userss = approved_users.find({})
+    for ch in approved_userss:
+        iid = ch["id"]
+        userss = ch["user"]
+    if event.is_group:
+        if (await is_register_admin(event.input_chat, event.message.sender_id)):
+            pass
+        elif event.chat_id == iid and event.sender_id == userss:
+            pass
+        else:
+            return
+    await event.reply("Processing ...wait")
