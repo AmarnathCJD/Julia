9904737beb02261bf84f1c069c80a35b6bcfc875
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-30 23:19:14 +0530

    Update test.py

--- julia/modules/test.py
+++ julia/modules/test.py
@@ -1,51 +1,19 @@
-from random import randint
-from PIL import ImageEnhance, ImageOps
-from random import uniform
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
+
 import asyncio
 import io
 import os
-import random
-import re
-import string
-import nltk
-from PIL import Image
-from zalgo_text import zalgo
 from julia.events import register
-import json
-import subprocess
-import textwrap
 import urllib.request
-from random import randrange
 from typing import List
 from typing import Optional
 import emoji
-from cowpy import cow
-from fontTools.ttLib import TTFont
-from PIL import ImageDraw
-from PIL import ImageFont
-from pymongo import MongoClient
 from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 from julia import *
-import pygments
 from countryinfo import CountryInfo
 import flag
-from pygments.lexers import Python3Lexer
-from pygments.formatters import ImageFormatter
-nltk.download("punkt")
-nltk.download("averaged_perceptron_tagger")
-
-WIDE_MAP = {i: i + 0xFEE0 for i in range(0x21, 0x7F)}
-WIDE_MAP[0x20] = 0x3000
-
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
-db = client["missjuliarobot"]
 approved_users = db.approve
 
 
@@ -82,99 +50,4 @@ async def msg(event):
             pass
         else:
             return
-    input_str = event.pattern_match.group(1)
-    lol = input_str
-    country = CountryInfo(lol)
-    try:
-	    a = country.info()
-    except:
-	    await event.reply("Country Not Avaiable Currently")
-    name = a.get("name")
-    bb= a.get("altSpellings")
-    hu = ''
-    for p in bb:
-    	hu += p+",  "
-	
-    area = a.get("area")
-    borders = ""
-    hell = a.get("borders")
-    for fk in hell:
-	    borders += fk+",  "
-	
-    call = "" 
-    WhAt = a.get("callingCodes")
-    for what in WhAt:
-	    call+= what+"  "
-	
-    capital = a.get("capital")
-    currencies = ""
-    fker = a.get("currencies")
-    for FKer in fker:
-	    currencies += FKer+",  "
-
-    HmM = a.get("demonym")
-    geo = a.get("geoJSON")
-    pablo = geo.get("features")
-    Pablo = pablo[0]
-    PAblo = Pablo.get("geometry")
-    EsCoBaR= PAblo.get("type")
-    iso = ""
-    iSo = a.get("ISO")
-    for hitler in iSo:
-      po = iSo.get(hitler)
-      iso += po+",  "
-    fla = iSo.get("alpha2")
-    nox = fla.upper()
-    okie = flag.flag(nox)
-
-    languages = a.get("languages")
-    lMAO=""
-    for lmao in languages:
-	    lMAO += lmao+",  "
-
-    nonive = a.get("nativeName")
-    waste = a.get("population")
-    reg = a.get("region")
-    sub = a.get("subregion")
-    tik = a.get("timezones")
-    tom =""
-    for jerry in tik:
-	    tom+=jerry+",   "
-
-    GOT = a.get("tld")
-    lanester = ""
-    for targaryen in GOT:
-	    lanester+=targaryen+",   "
-
-    wiki = a.get("wiki")
-
-    caption = f"""
-Country Name:- {name}
-Alternative Spellings:- {hu}
-Country Area:- {area} square kilometers
-Borders:- {borders}
-Calling Codes:- {call}
-Country's Capital:- {capital}
-Country's currency:- {currencies}
-Country's Flag:- {okie}
-Demonym:- {HmM}
-Country Type:- {EsCoBaR}
-ISO Names:- {iso}
-Languages:- {lMAO}
-Native Name:- {nonive}
-population:- {waste}
-Region:- {reg}
-Sub Region:- {sub}
-Time Zones:- {tom}
-Top Level Domain:- {lanester}
-wikipedia:- {wiki}
-"""
-    
-    
-    await tbot.send_message(
-        event.chat_id,
-        caption,
-        parse_mode="HTML",
-    )
-    
-    await event.delete()
+    await tbot.send_message("I'm gey")
