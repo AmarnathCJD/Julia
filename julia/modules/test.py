f65fe3f734a7c90131b10841069b93c2d24dde4d
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-30 22:31:05 +0530

    Update test.py

--- julia/modules/test.py
+++ julia/modules/test.py
@@ -26,11 +26,14 @@ from fontTools.ttLib import TTFont
 from PIL import ImageDraw
 from PIL import ImageFont
 from pymongo import MongoClient
-from telethon import *
-from telethon.tl import functions
-from telethon.tl.types import *
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
 from julia import *
 import pygments
+from countryinfo import CountryInfo
+import flag
 from pygments.lexers import Python3Lexer
 from pygments.formatters import ImageFormatter
 nltk.download("punkt")
@@ -66,17 +69,112 @@ async def is_register_admin(chat, user):
         )
     return None
 
-@register(pattern="^/test ?(.*)")
-async def coder_print(event):
-	a = await event.client.download_media(await event.get_reply_message(), Var.TEMP_DOWNLOAD_DIRECTORY)
-	s = open(a, 'r')
-	c = s.read()
-	s.close()
-	pygments.highlight(f"{c}", Python3Lexer(), ImageFormatter(font_name="DejaVu Sans Mono", line_numbers=True), "out.png")
-	res = await event.client.send_message(event.chat_id, "**Pasting this code on my page pls weitðŸ¤“...**", reply_to=event.reply_to_msg_id)
-	await event.client.send_file(event.chat_id, "out.png", force_document=True, reply_to=event.reply_to_msg_id)
-	await event.client.send_file(event.chat_id, "out.png", force_document=False, reply_to=event.reply_to_msg_id)
-	await res.delete()
-	await event.delete()
-	os.remove(a)
-	os.remove('out.png')
+@register(pattern="^/tast (.*)")
+async def msg(event):
+    approved_userss = approved_users.find({})
+    for ch in approved_userss:
+        iid = ch["id"]
+        userss = ch["user"]
+    if event.is_group:
+        if (await is_register_admin(event.input_chat, event.message.sender_id)):
+            pass
+        elif event.chat_id == iid and event.sender_id == userss:
+            pass
+        else:
+            return
+    input_str = event.pattern_match.group(1)
+    lol = input_str
+    country = CountryInfo(lol)
+    try:
+	    a = country.info()
+    except:
+	    await event.reply("Country Not Avaiable Currently")
+    name = a.get("name")
+    bb= a.get("altSpellings")
+    hu = ''
+    for p in bb:
+    	hu += p+",  "
+	
+    area = a.get("area")
+    borders = ""
+    hell = a.get("borders")
+    for fk in hell:
+	    borders += fk+",  "
+	
+    call = "" 
+    WhAt = a.get("callingCodes")
+    for what in WhAt:
+	    call+= what+"  "
+	
+    capital = a.get("capital")
+    currencies = ""
+    fker = a.get("currencies")
+    for FKer in fker:
+	    currencies += FKer+",  "
+
+    HmM = a.get("demonym")
+    geo = a.get("geoJSON")
+    pablo = geo.get("features")
+    Pablo = pablo[0]
+    PAblo = Pablo.get("geometry")
+    EsCoBaR= PAblo.get("type")
+    iso = ""
+    iSo = a.get("ISO")
+    for hitler in iSo:
+      po = iSo.get(hitler)
+      iso += po+",  "
+    fla = iSo.get("alpha2")
+    nox = fla.upper()
+    okie = flag.flag(nox)
+
+    languages = a.get("languages")
+    lMAO=""
+    for lmao in languages:
+	    lMAO += lmao+",  "
+
+    nonive = a.get("nativeName")
+    waste = a.get("population")
+    reg = a.get("region")
+    sub = a.get("subregion")
+    tik = a.get("timezones")
+    tom =""
+    for jerry in tik:
+	    tom+=jerry+",   "
+
+    GOT = a.get("tld")
+    lanester = ""
+    for targaryen in GOT:
+	    lanester+=targaryen+",   "
+
+    wiki = a.get("wiki")
+
+    caption = f"""
+Country Name:- {name}
+Alternative Spellings:- {hu}
+Country Area:- {area} square kilometers
+Borders:- {borders}
+Calling Codes:- {call}
+Country's Capital:- {capital}
+Country's currency:- {currencies}
+Country's Flag:- {okie}
+Demonym:- {HmM}
+Country Type:- {EsCoBaR}
+ISO Names:- {iso}
+Languages:- {lMAO}
+Native Name:- {nonive}
+population:- {waste}
+Region:- {reg}
+Sub Region:- {sub}
+Time Zones:- {tom}
+Top Level Domain:- {lanester}
+wikipedia:- {wiki}
+"""
+    
+    
+    await tbot.send_message(
+        event.chat_id,
+        caption,
+        parse_mode="HTML",
+    )
+    
+    await event.delete()
