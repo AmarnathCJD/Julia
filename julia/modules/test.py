094599790ad8cac73f0d08e8a53a604572155674
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-30 18:39:07 +0530

    Update test.py

--- julia/modules/test.py
+++ julia/modules/test.py
@@ -31,7 +31,8 @@ from telethon.tl import functions
 from telethon.tl.types import *
 from julia import *
 from julia.Config import Config
-
+import traceback
+from datetime import datetime
 nltk.download("punkt")
 nltk.download("averaged_perceptron_tagger")
 
@@ -67,7 +68,7 @@ async def is_register_admin(chat, user):
         )
     return None
 
-@register(pattern="^/test$")
+@register(pattern="^/test (.*)"))
 async def msg(event):
     approved_userss = approved_users.find({})
     for ch in approved_userss:
@@ -82,8 +83,54 @@ async def msg(event):
             return
         await event.reply("Processing ...wait")
         await asyncio.sleep(3)
-        if Config.GOOGLE_CHROME_BIN is None:
-        await event.reply("need to install Google Chrome. Module Stopping.")
-        else:
-        await event.reply("Chrome Configured")
+        start = datetime.now()
+    try:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--test-type")
+        chrome_options.add_argument("--headless")
+        # https://stackoverflow.com/a/53073789/4723940
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
+        await event.edit("Starting Google Chrome BIN")
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+        input_str = event.pattern_match.group(1)
+        imp = "anie"
+        driver.get(input_str)
+        await event.edit("Calculating Page Dimensions")
+        height = driver.execute_script(
+            "return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);"
+        )
+        width = driver.execute_script(
+            "return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);"
+        )
+        await event.edit("Painting web-page")
+        driver.set_window_size(width + 100, height + 100)
+        # Add some pixels on top of the calculated dimensions
+        # for good measure to make the scroll bars disappear
+        im_png = driver.get_screenshot_as_png()
+        # saves screenshot of entire page
+        driver.close()
+        await event.edit("Stopping Google Chrome BIN")
+        message_id = event.message.id
+        if event.reply_to_msg_id:
+            message_id = event.reply_to_msg_id
+        with io.BytesIO(im_png) as out_file:
+            out_file.name = "Anie.sshot.PNG"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                caption=imp,
+                force_document=True,
+                reply_to=message_id,
+                allow_cache=False,
+                silent=True,
+            )
+        end = datetime.now()
+        ms = (end - start).seconds
+        await event.edit(f"Done in {ms} seconds")
+    except Exception:
+        await event.edit(traceback.format_exc())
+
 
