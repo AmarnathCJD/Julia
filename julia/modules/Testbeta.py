5eea6f8dd3c10740904684d1cb9087390ce1b3e8
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-31 14:23:02 +0530

    Update Testbeta.py

--- julia/modules/Testbeta.py
+++ julia/modules/Testbeta.py
@@ -30,7 +30,13 @@ from telethon import *
 from telethon.tl import functions
 from telethon.tl.types import *
 from julia import *
-
+from countryinfo import CountryInfo
+import flag
+import html
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
 nltk.download("punkt")
 nltk.download("averaged_perceptron_tagger")
 
@@ -65,90 +71,106 @@ async def is_register_admin(chat, user):
     return None
 
 
-@register(pattern="^/co (.*)")
-async def msg(event):
-    approved_userss = approved_users.find({})
-    for ch in approved_userss:
-        iid = ch["id"]
-        userss = ch["user"]
-    if event.is_group:
-        if (await is_register_admin(event.input_chat, event.message.sender_id)):
-            pass
-        elif event.chat_id == iid and event.sender_id == userss:
-            pass
-        else:
-            return
+@register(pattern="^/country (.*)")
+async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    as_text = True
-    as_document = False
-    if input_str == "image":
-        as_document = False
-    elif input_str == "file":
-        as_document = True
-    elif input_str == "text":
-        as_text = True
-    await event.edit("`Calculating my internet speed. Please wait!`")
-    start = datetime.now()
-    s = speedtest.Speedtest()
-    s.get_best_server()
-    s.download()
-    s.upload()
-    end = datetime.now()
-    ms = (end - start).microseconds / 1000
-    response = s.results.dict()
-    download_speed = response.get("download")
-    upload_speed = response.get("upload")
-    ping_time = response.get("ping")
-    client_infos = response.get("client")
-    i_s_p = client_infos.get("isp")
-    i_s_p_rating = client_infos.get("isprating")
-    reply_msg_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_msg_id = event.reply_to_msg_id
+    lol = input_str
+    country = CountryInfo(lol)
     try:
-        response = s.results.share()
-        speedtest_image = response
-        if as_text:
-            await event.edit("""`SpeedTest completed in {} seconds`
+     a = country.info()
+    except:
+     await event.reply("Country Not Avaiable Currently")
+    name = a.get("name")
+    bb= a.get("altSpellings")
+    hu = ''
+    for p in bb:
+     hu += p+",  "
+ 
+    area = a.get("area")
+    borders = ""
+    hell = a.get("borders")
+    for fk in hell:
+     borders += fk+",  "
+ 
+    call = "" 
+    WhAt = a.get("callingCodes")
+    for what in WhAt:
+     call+= what+"  "
+ 
+    capital = a.get("capital")
+    currencies = ""
+    fker = a.get("currencies")
+    for FKer in fker:
+     currencies += FKer+",  "
 
-`Download: {}`
-`Upload: {}`
-`Ping: {}`
-`Internet Service Provider: {}`
-`ISP Rating: {}`""".format(ms, convert_from_bytes(download_speed), convert_from_bytes(upload_speed), ping_time, i_s_p, i_s_p_rating))
-        else:
-            await borg.send_file(
-                event.chat_id,
-                speedtest_image,
-                caption="**SpeedTest** completed in {} seconds".format(ms),
-                force_document=as_document,
-                reply_to=reply_msg_id,
-                allow_cache=False
-            )
-            await event.delete()
-    except Exception as exc:
-        await event.edit("""**SpeedTest** completed in {} seconds
-Download: {}
-Upload: {}
-Ping: {}
+    HmM = a.get("demonym")
+    geo = a.get("geoJSON")
+    pablo = geo.get("features")
+    Pablo = pablo[0]
+    PAblo = Pablo.get("geometry")
+    EsCoBaR= PAblo.get("type")
+    iso = ""
+    iSo = a.get("ISO")
+    for hitler in iSo:
+      po = iSo.get(hitler)
+      iso += po+",  "
+    fla = iSo.get("alpha2")
+    nox = fla.upper()
+    okie = flag.flag(nox)
 
-__With the Following ERRORs__
-{}""".format(ms, convert_from_bytes(download_speed), convert_from_bytes(upload_speed), ping_time, str(exc)))
+    languages = a.get("languages")
+    lMAO=""
+    for lmao in languages:
+     lMAO += lmao+",  "
 
+    nonive = a.get("nativeName")
+    waste = a.get("population")
+    reg = a.get("region")
+    sub = a.get("subregion")
+    tik = a.get("timezones")
+    tom =""
+    for jerry in tik:
+     tom+=jerry+",   "
 
-def convert_from_bytes(size):
-    power = 2**10
-    n = 0
-    units = {
-        0: "",
-        1: "kilobytes",
-        2: "megabytes",
-        3: "gigabytes",
-        4: "terabytes"
-    }
-    while size > power:
-        size /= power
-        n += 1
-    return f"{round(size, 2)} {units[n]}"
+    GOT = a.get("tld")
+    lanester = ""
+    for targaryen in GOT:
+     lanester+=targaryen+",   "
+
+    wiki = a.get("wiki")
+
+    caption = f"""<b><u>information gathered successfully</b></u>
+<b>
+Country Name:- {name}
+Alternative Spellings:- {hu}
+Country Area:- {area} square kilometers
+Borders:- {borders}
+Calling Codes:- {call}
+Country's Capital:- {capital}
+Country's currency:- {currencies}
+Country's Flag:- {okie}
+Demonym:- {HmM}
+Country Type:- {EsCoBaR}
+ISO Names:- {iso}
+Languages:- {lMAO}
+Native Name:- {nonive}
+population:- {waste}
+Region:- {reg}
+Sub Region:- {sub}
+Time Zones:- {tom}
+Top Level Domain:- {lanester}
+wikipedia:- {wiki}</b>
+
+<u><b>
+Information Gathered By Friday.
+Get Your Own Friday From @FRIDAYCHAT.</b></u>
+"""
+    await borg.send_message(
+        event.chat_id,
+        caption,
+        parse_mode="HTML",
+    )
+    
+    await event.delete()
