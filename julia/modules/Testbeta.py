b7d92f039696ef7df8db3b76760b1cc67638f0a3
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-31 14:29:24 +0530

    Update Testbeta.py

--- julia/modules/Testbeta.py
+++ julia/modules/Testbeta.py
@@ -1,4 +1,5 @@
 from random import randint
+#made by RoseLoverX Jo Kang Kiya Wo Mera beta Ho
 from PIL import ImageEnhance, ImageOps
 from random import uniform
 from selenium.webdriver.chrome.options import Options
@@ -30,6 +31,11 @@ from telethon import *
 from telethon.tl import functions
 from telethon.tl.types import *
 from julia import *
+from julia.Config import Config
+import traceback
+from datetime import datetime
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
 from countryinfo import CountryInfo
 import flag
 import html
@@ -37,9 +43,6 @@ from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
-nltk.download("punkt")
-nltk.download("averaged_perceptron_tagger")
-
 WIDE_MAP = {i: i + 0xFEE0 for i in range(0x21, 0x7F)}
 WIDE_MAP[0x20] = 0x3000
 
@@ -49,6 +52,8 @@ client = MongoClient(MONGO_DB_URI)
 db = client["missjuliarobot"]
 approved_users = db.approve
 
+
+GOOGLE_CHROME_BIN = "/app/.apt/usr/bin/google-chrome"
 
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
@@ -70,107 +75,21 @@ async def is_register_admin(chat, user):
         )
     return None
 
-
-@register(pattern="^/country (.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    lol = input_str
-    country = CountryInfo(lol)
+@register(pattern="^/test (.*)")
+async def msg(event):
+    approved_userss = approved_users.find({})
+    for ch in approved_userss:
+        iid = ch["id"]
+        userss = ch["user"]
+    if event.is_group:
+        if (await is_register_admin(event.input_chat, event.message.sender_id)):
+            pass
+        elif event.chat_id == iid and event.sender_id == userss:
+            pass
+        else:
+            return
+        await event.reply("Painting web-page..")
+    start = datetime.now()
     try:
-     a = country.info()
-    except:
-     await event.reply("Country Not Avaiable Currently")
-    name = a.get("name")
-    bb= a.get("altSpellings")
-    hu = ''
-    for p in bb:
-     hu += p+",  "
- 
-    area = a.get("area")
-    borders = ""
-    hell = a.get("borders")
-    for fk in hell:
-     borders += fk+",  "
- 
-    call = "" 
-    WhAt = a.get("callingCodes")
-    for what in WhAt:
-     call+= what+"  "
- 
-    capital = a.get("capital")
-    currencies = ""
-    fker = a.get("currencies")
-    for FKer in fker:
-     currencies += FKer+",  "
-
-    HmM = a.get("demonym")
-    geo = a.get("geoJSON")
-    pablo = geo.get("features")
-    Pablo = pablo[0]
-    PAblo = Pablo.get("geometry")
-    EsCoBaR= PAblo.get("type")
-    iso = ""
-    iSo = a.get("ISO")
-    for hitler in iSo:
-      po = iSo.get(hitler)
-      iso += po+",  "
-    fla = iSo.get("alpha2")
-    nox = fla.upper()
-    okie = flag.flag(nox)
-
-    languages = a.get("languages")
-    lMAO=""
-    for lmao in languages:
-     lMAO += lmao+",  "
-
-    nonive = a.get("nativeName")
-    waste = a.get("population")
-    reg = a.get("region")
-    sub = a.get("subregion")
-    tik = a.get("timezones")
-    tom =""
-    for jerry in tik:
-     tom+=jerry+",   "
-
-    GOT = a.get("tld")
-    lanester = ""
-    for targaryen in GOT:
-     lanester+=targaryen+",   "
-
-    wiki = a.get("wiki")
-
-    caption = f"""<b><u>information gathered successfully</b></u>
-<b>
-Country Name:- {name}
-Alternative Spellings:- {hu}
-Country Area:- {area} square kilometers
-Borders:- {borders}
-Calling Codes:- {call}
-Country's Capital:- {capital}
-Country's currency:- {currencies}
-Country's Flag:- {okie}
-Demonym:- {HmM}
-Country Type:- {EsCoBaR}
-ISO Names:- {iso}
-Languages:- {lMAO}
-Native Name:- {nonive}
-population:- {waste}
-Region:- {reg}
-Sub Region:- {sub}
-Time Zones:- {tom}
-Top Level Domain:- {lanester}
-wikipedia:- {wiki}</b>
-
-<u><b>
-Information Gathered By Friday.
-Get Your Own Friday From @FRIDAYCHAT.</b></u>
-"""
-    await borg.send_message(
-        event.chat_id,
-        caption,
-        parse_mode="HTML",
-    )
-    
-    await event.delete()
+        input_str = event.pattern_match.group(1)
+        
