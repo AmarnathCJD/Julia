76e883c433e886f86ccbbdbf26a28fec251e44de
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-02 20:50:15 +0530

    Create Filemgr.py

---
+++ julia/modules/Filemgr.py
@@ -0,0 +1,172 @@
+#made by RoseLoverX Jo Kang Kiya Wo Mera beta Ho
+from PIL import ImageEnhance, ImageOps
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import re
+import string
+import nltk
+from zalgo_text import zalgo
+from julia.events import register
+import json
+import subprocess
+import textwrap
+import urllib.request
+from random import randrange
+from typing import List
+from typing import Optional
+import emoji
+from fontTools.ttLib import TTFont
+from pymongo import MongoClient
+from telethon import *
+from telethon.tl import functions
+from telethon.tl.types import *
+from julia import *
+from julia.Config import Config
+import traceback
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+WIDE_MAP = {i: i + 0xFEE0 for i in range(0x21, 0x7F)}
+WIDE_MAP[0x20] = 0x3000
+from PIL import Image
+from telegraph import Telegraph, exceptions, upload_file
+import requests
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI)
+db = client["missjuliarobot"]
+approved_users = db.approve
+import io
+import os
+import os.path
+import time
+from os.path import exists, isdir
+import sys
+import asyncio
+from datetime import datetime
+from pathlib import Path
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await
+             tbot(functions.channels.GetParticipantRequest(chat,
+                                                           user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await tbot.get_peer_id(user)
+        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id)
+                         )).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
+
+@register(pattern="^/ls ?(.*)")
+async def msg(event):
+    approved_userss = approved_users.find({})
+    for ch in approved_userss:
+        iid = ch["id"]
+        userss = ch["user"]
+    if event.is_group:
+        if (await is_register_admin(event.input_chat, event.message.sender_id)):
+            pass
+        elif event.chat_id == iid and event.sender_id == userss:
+            pass
+        else:
+            return
+    cat = event.pattern_match.group(1)
+    path = cat if cat else os.getcwd()
+    if not exists(path):
+        await event.edit(
+            f"There is no such directory or file with the name {cat}.\n Pleasecheck again!"
+        )
+        return
+    if isdir(path):
+        msg = "**PATH:** {}\n\n".format(path) if cat else "• File Manager •\n\n"
+        lists = os.listdir(path)
+        files = ""
+        folders = ""
+        for contents in sorted(lists):
+            catpath = path + "/" + contents
+            if not isdir(catpath):
+                size = os.stat(catpath).st_size
+                if contents.endswith((".mp3", ".flac", ".wav", ".m4a")):
+                    files += "🎵 " + f"{contents}\n"
+                if contents.endswith((".opus")):
+                    files += "🎙 " + f"{contents}\n"
+                elif contents.endswith(
+                    (".mkv", ".mp4", ".webm", ".avi", ".mov", ".flv")
+                ):
+                    files += "🎞 " + f"{contents}\n"
+                elif contents.endswith(
+                    (".zip", ".tar", ".tar.gz", ".rar", ".7z", ".xz")
+                ):
+                    files += "🗜 " + f"{contents}\n"
+                elif contents.endswith(
+                    (".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ".webp")
+                ):
+                    files += "🖼 " + f"{contents}\n"
+                elif contents.endswith((".exe", ".deb")):
+                    files += "⚙️ " + f"{contents}\n"
+                elif contents.endswith((".iso", ".img")):
+                    files += "💿 " + f"{contents}\n"
+                elif contents.endswith((".apk", ".xapk")):
+                    files += "📱 " + f"{contents}\n"
+                elif contents.endswith((".py")):
+                    files += "❄️ " + f"{contents}\n"
+                else:
+                    files += "📄 " + f"{contents}\n"
+            else:
+                folders += f"📂 {contents}\n"
+        msg = msg + folders + files if files or folders else msg + "__empty path__"
+    else:
+        size = os.stat(path).st_size
+        msg = "The details of given file :\n\n"
+        if path.endswith((".mp3", ".flac", ".wav", ".m4a")):
+            mode = "🎵 "
+        if path.endswith((".opus")):
+            mode = "🎙 "
+        elif path.endswith((".mkv", ".mp4", ".webm", ".avi", ".mov", ".flv")):
+            mode = "🎞 "
+        elif path.endswith((".zip", ".tar", ".tar.gz", ".rar", ".7z", ".xz")):
+            mode = "🗜 "
+        elif path.endswith((".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ".webp")):
+            mode = "🖨️ "
+        elif path.endswith((".exe", ".deb")):
+            mode = "⚙️ "
+        elif path.endswith((".iso", ".img")):
+            mode = "💿 "
+        elif path.endswith((".apk", ".xapk")):
+            mode = "📱 "
+        elif path.endswith((".py")):
+            mode = "❄️ "
+        else:
+            mode = "🍨 "
+        time.ctime(os.path.getctime(path))
+        time2 = time.ctime(os.path.getmtime(path))
+        time3 = time.ctime(os.path.getatime(path))
+        msg += f"**Location :** {path}\n"
+        msg += f"**Icon :** {mode}\n"
+        msg += f"**Size :** {mode}\n"
+        msg += f"**Last Modified Time:** {time2}\n"
+        msg += f"**Last Accessed Time:** {time3}"
+
+    if len(msg) > MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(msg)) as out_file:
+            out_file.name = "ls.txt"
+            await event.client.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=path,
+            )
+            await event.delete()
+    else:
+        await event.reply(msg)
