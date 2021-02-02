3a015039eb98f920c5c1b63eba6ae21df514eed4
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-02 21:25:19 +0530

    Update Filemgr.py

--- julia/modules/Filemgr.py
+++ julia/modules/Filemgr.py
@@ -67,7 +67,7 @@ async def is_register_admin(chat, user):
         )
     return None
 
-@register(pattern="^/ls ?(.*)")
+@register(pattern="^/restart$")
 async def msg(event):
     approved_userss = approved_users.find({})
     for ch in approved_userss:
@@ -80,93 +80,14 @@ async def msg(event):
             pass
         else:
             return
-    cat = event.pattern_match.group(1)
-    path = cat if cat else os.getcwd()
-    if not exists(path):
-        await event.reply(
-            f"There is no such directory or file with the name {cat}.\n Pleasecheck again!"
-        )
-        return
-    if isdir(path):
-        msg = "**PATH:** {}\n\n".format(path) if cat else "• File Manager •\n\n"
-        lists = os.listdir(path)
-        files = ""
-        folders = ""
-        for contents in sorted(lists):
-            catpath = path + "/" + contents
-            if not isdir(catpath):
-                size = os.stat(catpath).st_size
-                if contents.endswith((".mp3", ".flac", ".wav", ".m4a")):
-                    files += "🎵 " + f"{contents}\n"
-                if contents.endswith((".opus")):
-                    files += "🎙 " + f"{contents}\n"
-                elif contents.endswith(
-                    (".mkv", ".mp4", ".webm", ".avi", ".mov", ".flv")
-                ):
-                    files += "🎞 " + f"{contents}\n"
-                elif contents.endswith(
-                    (".zip", ".tar", ".tar.gz", ".rar", ".7z", ".xz")
-                ):
-                    files += "🗜 " + f"{contents}\n"
-                elif contents.endswith(
-                    (".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ".webp")
-                ):
-                    files += "🖼 " + f"{contents}\n"
-                elif contents.endswith((".exe", ".deb")):
-                    files += "⚙️ " + f"{contents}\n"
-                elif contents.endswith((".iso", ".img")):
-                    files += "💿 " + f"{contents}\n"
-                elif contents.endswith((".apk", ".xapk")):
-                    files += "📱 " + f"{contents}\n"
-                elif contents.endswith((".py")):
-                    files += "❄️ " + f"{contents}\n"
-                else:
-                    files += "📄 " + f"{contents}\n"
-            else:
-                folders += f"📂 {contents}\n"
-        msg = msg + folders + files if files or folders else msg + "__empty path__"
-    else:
-        size = os.stat(path).st_size
-        msg = "The details of given file :\n\n"
-        if path.endswith((".mp3", ".flac", ".wav", ".m4a")):
-            mode = "🎵 "
-        if path.endswith((".opus")):
-            mode = "🎙 "
-        elif path.endswith((".mkv", ".mp4", ".webm", ".avi", ".mov", ".flv")):
-            mode = "🎞 "
-        elif path.endswith((".zip", ".tar", ".tar.gz", ".rar", ".7z", ".xz")):
-            mode = "🗜 "
-        elif path.endswith((".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ".webp")):
-            mode = "🖨️ "
-        elif path.endswith((".exe", ".deb")):
-            mode = "⚙️ "
-        elif path.endswith((".iso", ".img")):
-            mode = "💿 "
-        elif path.endswith((".apk", ".xapk")):
-            mode = "📱 "
-        elif path.endswith((".py")):
-            mode = "❄️ "
-        else:
-            mode = "🍨 "
-        time.ctime(os.path.getctime(path))
-        time2 = time.ctime(os.path.getmtime(path))
-        time3 = time.ctime(os.path.getatime(path))
-        msg += f"**Location :** {path}\n"
-        msg += f"**Icon :** {mode}\n"
-        msg += f"**Size :** {mode}\n"
-        msg += f"**Last Modified Time:** {time2}\n"
-        msg += f"**Last Accessed Time:** {time3}"
-
-    if len(msg) > MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(msg)) as out_file:
-            out_file.name = "ls.txt"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=path,
-            )
-            await event.delete()
-    else:
-        await event.reply(msg)
+    await asyncio.sleep(2)
+    await event.reply("Restarting  ▰▰▰▱▱▱▱▱▱▱...")
+    await asyncio.sleep(2)
+    await event.reply("Restarting  ▰▰▰▰▰▰▰▰▱▱..")
+    await asyncio.sleep(2)
+    await event.reply(
+        "Restarted,wait... `.ping` me or type `.help`  to check if your anie userbot is online/alive "
+    )
+    await tbot.disconnect()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
