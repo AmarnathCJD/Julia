9ff234910b4f45c72272a9317434190d20fcd4fa
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-02 22:21:30 +0530

    Update Filemanager.py

--- julia/modules/Filemanager.py
+++ julia/modules/Filemanager.py
@@ -6,6 +6,26 @@ import asyncio
 import os
 import sys
 
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await
+             tbot(functions.channels.GetParticipantRequest(chat,
+                                                           user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await tbot.get_peer_id(user)
+        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id)
+                         )).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
+
 @register(pattern="^/restart$")
 async def msg(event):
     check = ups.message.sender_id
