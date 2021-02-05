355fe6755c2c7ba1394b60115f78a05cf709b0a1
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-05 21:28:01 +0530

    Update Ok.py

--- julia/modules/Ok.py
+++ julia/modules/Ok.py
@@ -112,16 +112,22 @@ async def _(event):
     await event.delete()
     await a.delete()
 
-async def drawText(file, text):
-    img = Image.open(file)
-    os.remove(file)
-    await event.reply("Usage:- memify upper text ; lower text")
+async def drawText(image_path, text):
+    img = Image.open(image_path)
+    os.remove(image_path)
     i_width, i_height = img.size
+    if os.name == "nt":
+        fnt = "arial.ttf"
+    else:
+        fnt = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
-    upper_text = text
+    if ";" in text:
+        upper_text, lower_text = text.split(";")
+    else:
+        upper_text = text
+        lower_text = ""
     draw = ImageDraw.Draw(img)
     current_h, pad = 10, 5
-    await event.reply("Usage:- memify upper text ; lower text")
     if upper_text:
         for u_text in textwrap.wrap(upper_text, width=15):
             u_width, u_height = draw.textsize(u_text, font=m_font)
@@ -157,8 +163,57 @@ async def drawText(file, text):
                 fill=(255, 255, 255),
             )
             current_h += u_height + pad
-    await event.reply("Usage:- memify upper tfukkext ; lower text")
+    if lower_text:
+        for l_text in textwrap.wrap(lower_text, width=15):
+            u_width, u_height = draw.textsize(l_text, font=m_font)
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) - 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) + 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((20 / 640) * i_width)) - 2,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((20 / 640) * i_width)) + 2,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+            current_h += u_height + pad
     image_name = "memify.webp"
-    webp_file = os.path.join(Config.TEMP_DOWNLOAD_DIRECTORY, image_name)
+    webp_file = os.path.join(TEMP_DOWNLOAD_DIRECTORY, image_name)
     img.save(webp_file, "webp")
     return webp_file
