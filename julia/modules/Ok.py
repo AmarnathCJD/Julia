4e380b4f4c2955cf657d14fa01fea4bdcb9ecee2
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-05 19:36:23 +0530

    Update Ok.py

--- julia/modules/Ok.py
+++ julia/modules/Ok.py
@@ -54,20 +54,33 @@ async def _(event):
     danish = cyber.credit_card_full()
     await event.reply(f"ռaʍɛ:-\n`{killer}`\n\naɖɖʀɛss:-\n`{kali}`\n\nᴄaʀɖ:-\n`{danish}`")
 
-@register(pattern="^/gban")
+@register(pattern="^/iplookup (.*)")
 async def _(event):
-    if event.fwd_from:
-        return
-    approved_userss = approved_users.find({})
-    for ch in approved_userss:
-        iid = ch["id"]
-        userss = ch["user"]
     if event.is_group:
-        if await is_register_admin(event.input_chat, event.message.sender_id):
-            pass
-        elif event.chat_id == iid and event.sender_id == userss:
-            pass
-        else:
-            return
-    await event.reply(f"Gban Initiating...")
-    await event.reply(f"Cant Remove User Is Admin")
+        pass
+    else:
+        return
+    input_str = event.pattern_match.group(1)
+
+    adress = input_str
+
+    token = "19e7f2b6fe27deb566140aae134dec6b"
+
+    api = "http://api.ipstack.com/" + adress + "?access_key=" + token + "&format=1"
+
+    result = urllib.request.urlopen(api).read()
+    result = result.decode()
+
+    result = json.loads(result)
+    a = result["type"]
+    b = result["country_code"]
+    c = result["region_name"]
+    d = result["city"]
+    e = result["zip"]
+    f = result["latitude"]
+    g = result["longitude"]
+    await event.reply(
+        f"<b><u>INFORMATION GATHERED SUCCESSFULLY</b></u>\n<b>IP:-</b><code>{input_str}</code>\n\n<b>Ip type :-</b><code>{a}</code>\n<b>Country code:- </b> <code>{b}</code>\n<b>State name :-</b><code>{c}</code>\n<b>City name :- </b><code>{d}</code>\n<b>zip :-</b><code>{e}</code>\n<b>Latitude:- </b> <code>{f}</code>\n<b>Longitude :- </b><code>{g}</code>\n",
+        parse_mode="HTML",
+    )
+    
