ae80be7009a4a6b18ca0e3af0c7d266fb35c5a1c
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-05 17:59:07 +0530

    Update exec.py

--- julia/modules/exec.py
+++ julia/modules/exec.py
@@ -76,6 +76,7 @@ async def msg(event):
     elif event.sender_id == OWNER_ID:
         pass
     else:
+    await event.reply("This is a developer restricted command. You do not have permissions to run this.")
         return
     PROCESS_RUN_TIME = 100
     cmd = event.pattern_match.group(1)
