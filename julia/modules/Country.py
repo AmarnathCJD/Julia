fe09d030b71e6a1d9b04b8f67b3d741942df74bd
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-07 21:22:54 +0530

    Update Country.py

--- julia/modules/Country.py
+++ julia/modules/Country.py
@@ -181,5 +181,41 @@ Top Level Domain:- {lanester}
     )
     
     await event.delete()
-
-        
+import logging
+import asyncio
+from telethon import TelegramClient, events
+@register(pattern="^/frwd")
+async def frwder(event):
+    if event.is_private:
+        await event.reply("I work in groups!")
+        return
+    ok = await msgfrwd(GetFullUserRequest(event.sender_id))
+    txt = event.text.split(" ", maxsplit=2)
+    try:
+        chat = txt[1]
+        msg = txt[2]
+        if msg is None:
+            await event.reply("No message provided!\n\nFormat - `/frwd <chat id/username> <message/reply to message>`")
+            return
+        if chat.startswith('@'):
+            try:
+                temp = await msgfrwd.get_entity(chat)
+                chat = temp.id
+            except UsernameNotOccupiedError as e:
+                await event.reply(str(e))
+                return
+        try:
+            sent = await msgfrwd.send_message(chat, msg)
+            await sent.reply(f"Message from [{ok.user.first_name}](tg://user?id={event.sender_id})")
+            temp = await event.reply("Done!")
+            await asyncio.sleep(10)
+            #await event.delete()
+            await temp.delete()
+        except Exception as e:
+            await event.reply(f"Bot not in the group 🤔\n\n{str(e)}")
+    except UsernameNotOccupiedError as e:
+        await event.reply(str(e))
+        return
+    except Exception as e:
+        await event.reply(f"Format - `/frwd <chat id/username> <message/reply to message>`\n\n{str(e)}")
+        return
