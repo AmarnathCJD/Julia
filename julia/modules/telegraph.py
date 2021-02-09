5e194627083233eaefe87f6ab7f6d5e3f3b85ddd
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-09 16:53:11 +0530

    Update telegraph.py

--- julia/modules/telegraph.py
+++ julia/modules/telegraph.py
@@ -14,7 +14,7 @@ r = telegraph.create_account(short_name=babe)
 auth_url = r["auth_url"]
 
 
-@register(pattern="t(m|t) ?(.*)")
+@register(pattern="^/t(m|t) ?(.*)")
 async def _(event):
     if event.fwd_from:
         return
