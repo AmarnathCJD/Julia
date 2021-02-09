2c88917e96b3beac5cc1d852f279f3c71b44313c
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-09 12:17:56 +0530

    Update Config.py

--- julia/Config.py
+++ julia/Config.py
@@ -6,6 +6,7 @@ if ENV:
     class Config(object):
         CHROME_DRIVER = os.environ.get("CHROME_DRIVER", "/app/.chromedriver/bin/chromedriver")
         GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
+        TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
 else:
     class Config(object):
         DB_URI = None
