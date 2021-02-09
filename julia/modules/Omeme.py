6efc66503c76a4cc7c20d160a57aa8fd7eda1be2
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-09 10:56:03 +0530

    Update Omeme.py

--- julia/modules/Omeme.py
+++ julia/modules/Omeme.py
@@ -475,3 +475,45 @@ async def upload_file(message):
             allow_cache=False,
             reply_to=message.message_id
         )
+DANISH = "58199388-5499-4c98-b052-c679b16310f9"
+from telethon.tl.types import MessageMediaPhoto
+import os, urllib, requests, asyncio
+@register(pattern="^/co")
+async def _(event):
+          
+    reply = await event.get_reply_message()
+    if not reply:#By @Danish_00
+#Fixed By a NOOB
+        return await event.reply(
+           "Reply to any image or non animated sticker !"
+        )
+    devent = await event.reply("Downloading the file to check...")
+    media = await event.client.download_media(reply)
+    if not media.endswith(("png", "jpg", "webp")):
+        return await event.reply(
+             "Reply to any image or non animated sticker !"
+        )#By @Danish_00
+#Fixed By a NOOB
+    await dcevent.edit("coloring image sar...")
+    r = requests.post(
+        "https://api.deepai.org/api/colorizer",
+        files={
+            "image": open(media, "rb"),
+        },
+        headers={"api-key": DANISH},
+    )#By @Danish_00
+#Fixed By a NOOB
+    os.remove(media)
+    if "status" in r.json():
+        return await devent.edit( r.json()["status"])
+    r_json = r.json()["output_url"]
+    pic_id = r.json()["id"]
+    
+    link = f"https://api.deepai.org/job-view-file/{pic_id}/inputs/image.jpg"
+    result = f"{r_json}"
+    
+    await devent.delete()
+    await borg.send_message(
+        event.chat_id,
+        file=result
+    )
