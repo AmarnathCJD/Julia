206acd23a32d43b8fd83cb56c243ed9c65efee37
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-09 12:52:26 +0530

    Update exec.py

--- julia/modules/exec.py
+++ julia/modules/exec.py
@@ -1,5 +1,4 @@
 #made by RoseLoverX Jo Kang Kiya Wo Mera beta Ho
-from PIL import ImageEnhance, ImageOps
 from random import uniform
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
@@ -7,8 +6,6 @@ import asyncio
 import random
 import re
 import string
-import nltk
-from zalgo_text import zalgo
 from julia.events import register
 import json
 import subprocess
@@ -17,61 +14,29 @@ import urllib.request
 from random import randrange
 from typing import List
 from typing import Optional
-import emoji
 from julia import OWNER_ID
 from julia import SUDO_USERS
-from cowpy import cow
 from fontTools.ttLib import TTFont
-from pymongo import MongoClient
 from telethon import *
 from telethon.tl import functions
 from telethon.tl.types import *
 from julia import *
 from julia.Config import Config
 import traceback
-nltk.download("punkt")
-nltk.download("averaged_perceptron_tagger")
 from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
-WIDE_MAP = {i: i + 0xFEE0 for i in range(0x21, 0x7F)}
-WIDE_MAP[0x20] = 0x3000
 import os
 from datetime import datetime
-from PIL import Image
-from telegraph import Telegraph, exceptions, upload_file
 import requests
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
-db = client["missjuliarobot"]
-approved_users = db.approve
 import io
 import sys
 import traceback
-async def is_register_admin(chat, user):
-    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-
-        return isinstance(
-            (await
-             tbot(functions.channels.GetParticipantRequest(chat,
-                                                           user))).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
-        )
-    if isinstance(chat, types.InputPeerChat):
-
-        ui = await tbot.get_peer_id(user)
-        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id)
-                         )).full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
-        )
-    return None
 
 @register(pattern="^/exec (.*)")
 async def msg(event):
-    elif event.sender_id == OWNER_ID:
+    if event.sender_id == OWNER_ID:
         pass
     elif event.sender_id not in SUDO_USERS:
         await event.reply("This is an owner restricted command. You do not have permissions to run this.")
