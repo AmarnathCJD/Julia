adfcfb7cb9e69bbb5a4d705840cce87edceb0ecf
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-10 19:01:53 +0530

    Update AFK_😴.py

--- julia/modules/AFK_😴.py
+++ julia/modules/AFK_😴.py
@@ -25,15 +25,6 @@ async def is_register_admin(chat, user):
             ).participant,
             (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
         )
-    if isinstance(chat, types.InputPeerChat):
-        ui = await tbot.get_peer_id(user)
-        ps = (
-            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
-        ).full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
-        )
     if isinstance(chat, types.InputPeerUser):          
         return True
 
