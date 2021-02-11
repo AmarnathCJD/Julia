f4c1ac5ee11c76bed3d23f454353af7d4360dd01
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-11 22:43:49 +0530

    Update image_vidtools.py

--- julia/modules/image_vidtools.py
+++ julia/modules/image_vidtools.py
@@ -4,30 +4,30 @@ from julia import CMD_HELP
 from julia import tbot as borg
 from julia import TEMP_DOWNLOAD_DIRECTORY
 import os
+import wget
 from shutil import rmtree
 import cv2
-import io
 import cv2 as cv
 import random
 import numpy as np
 from datetime import datetime
-import time
 from PIL import Image, ImageDraw, ImageFont
 import pytz 
 import asyncio
 import requests
 from PIL import Image, ImageDraw, ImageFont
+from telegraph import upload_file
+import time
 import html
 from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
-from julia.func import runcmd, convert_to_image, progress, humanbytes
-from julia.Ok import upload_file as uf
-sedpath = "./starkz/"
+sedpath = "./starkgangz/"
 if not os.path.isdir(sedpath):
     os.makedirs(sedpath)
-
+from julia.Ok import upload_file as if
+from julia.func import runcmd, convert_to_image, progress, humanbytes
 
 @register(pattern="^/cit")
 async def hmm(event):
