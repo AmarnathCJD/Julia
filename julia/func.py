3292f6cb6c95e7efedde86c1fd24651d0e1b45d0
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-11 17:19:15 +0530

    Update func.py

--- julia/func.py
+++ julia/func.py
@@ -44,6 +44,20 @@ async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
         process.pid,
     )
 
+def humanbytes(size):
+    """Input size in bytes,
+    outputs in a human readable format"""
+    # https://stackoverflow.com/a/49361727/4723940
+    if not size:
+        return ""
+    # 2 ** 10 = 1024
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
 
 
 async def progress(current, total, event, start, type_of_ps, file_name=None):
