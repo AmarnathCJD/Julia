155abccc0216b55a655a901a78ec8f3f8caae495
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-11 11:49:41 +0530

    Update glitch.py

--- julia/modules/glitch.py
+++ julia/modules/glitch.py
@@ -10,12 +10,10 @@ from glitch_this import ImageGlitcher
 from PIL import Image
 from telethon import functions, types
 
-@register(pattern="(glitch|glitchs)(?: |$)(.*)")
+@register(pattern="^/gl")
 async def glitch(cat):
     if cat.fwd_from:
         return
-    cmd = cat.pattern_match.group(1)
-    catinput = cat.pattern_match.group(2)
     reply = await cat.get_reply_message()
     if not reply:
         return await cat.reply("`Reply to supported Media...`")
@@ -23,16 +21,7 @@ async def glitch(cat):
     san = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
     if not os.path.isdir("./temp"):
         os.mkdir("./temp")
-    if catinput:
-        if not catinput.isdigit():
-            await cat.reply("`You input is invalid, check help`")
-            return
-        catinput = int(catinput)
-        if not 0 < catinput < 9:
-            await cat.edit("`Invalid Range...`")
-            return
-    else:
-        catinput = 2
+    catinput = 2
     glitch_file = await _cattools.media_to_pic(cat, reply)
     try:
         san = Get(san)
@@ -41,26 +30,12 @@ async def glitch(cat):
         pass
     glitcher = ImageGlitcher()
     img = Image.open(glitch_file[1])
-    if cmd == "glitchs":
-        glitched = os.path.join("./temp", "glitched.webp")
-        glitch_img = glitcher.glitch_image(img, catinput, color_offset=True)
-        glitch_img.save(glitched)
-        await cat.client.send_file(cat.chat_id, glitched, reply_to=catid)
-    elif cmd == "glitch":
-        glitched = os.path.join("./temp", "glitched.gif")
-        glitch_img = glitcher.glitch_image(img, catinput, color_offset=True, gif=True)
-        DURATION = 200
-        LOOP = 0
-        glitch_img[0].save(
-            glitched,
-            format="GIF",
-            append_images=glitch_img[1:],
-            save_all=True,
-            duration=DURATION,
-            loop=LOOP,
-        )
-        sandy = await cat.client.send_file(cat.chat_id, glitched, reply_to=catid)
-        await _catutils.unsavegif(cat, sandy)
+    glitched = os.path.join("./temp", "glitched.webp")
+    glitch_img = glitcher.glitch_image(img, catinput, color_offset=True)
+    glitch_img.save(glitched)
+    await cat.client.send_file(cat.chat_id, glitched, reply_to=catid)
+    sandy = await cat.tbot.send_file(cat.chat_id, glitched, reply_to=catid)
+    await _catutils.unsavegif(cat, sandy)
     await glitch_file[0].delete()
     for files in (glitch_file[1], glitched):
         if files and os.path.exists(files):
