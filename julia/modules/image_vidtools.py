409e639e34e837d75f4090ab8c6b1e5a8be11d39
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-11 16:39:43 +0530

    Update image_vidtools.py

--- julia/modules/image_vidtools.py
+++ julia/modules/image_vidtools.py
@@ -15,8 +15,12 @@ import pytz
 import asyncio
 import requests
 from PIL import Image, ImageDraw, ImageFont
-
-
+import html
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+from julia.functions import runcmd, convert_to_image
 sedpath = "./starkgangz/"
 if not os.path.isdir(sedpath):
     os.makedirs(sedpath)
