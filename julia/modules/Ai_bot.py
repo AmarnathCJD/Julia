b0151385a58587773ae00d35ab42d33659b45c97
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-13 20:28:23 +0530

    Create Ai_bot.py

---
+++ julia/modules/Ai_bot.py
@@ -0,0 +1,36 @@
+import aiohttp
+import asyncio
+import re
+from julia import TOKEN as bot_token
+from julia import OWNER_ID as owner_id
+from julia import BOT_ID as bot_id
+from pyrogram import Client, filters
+
+luna = Client(
+    ":memory:",
+    bot_token=bot_token,
+    api_id=6,
+    api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e",
+)
+
+blacklisted = []
+
+
+async def getresp(query):
+    url = f"https://lunabot.tech/?query={query}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as res:
+            res = await res.json()
+            text = res["response"]
+            return text
+
+@luna.on_message(filters.command("halp"))
+async def start(_, message):
+    user_id = message.from_user.id
+    if user_id in blacklisted:
+        return
+    await luna.send_chat_action(message.chat.id, "typing")
+    await message.reply_text(
+        "**Only For Owners**\n/shutdown - `Shutdown Luna.`\n/blacklist - `Blacklist A User.`\n/whitelist - `Whitelist A User.`"
+    )
+
