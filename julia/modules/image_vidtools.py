c2e3f608b81b87cbbb0768c87e1294f233666e54
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-15 10:40:44 +0530

    Update image_vidtools.py

--- julia/modules/image_vidtools.py
+++ julia/modules/image_vidtools.py
@@ -135,3 +135,29 @@ async def slogo(event):
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
     if os.path.exists(fname2):
             os.remove(fname2)
+
+@register(pattern="^/(blogo|betalogo) ?(.*)")
+async def slogo(event):
+    if event.fwd_from:
+        return
+    await event.edit("`Processing..`")
+    text = event.pattern_match.group(2)
+    img = Image.open('./resources/IMG_20210215_103846_312.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "white"
+    shadowcolor = "black"
+    font = ImageFont.truetype("./resources/Vampire Wars.otf", 380)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= (image_heightz-h)/2
+    draw.text((x, y), text, font=font, fill="white", stroke_width=30, stroke_fill="black")
+    fname2 = "LogoBy@FRIDAYOT.png"
+    img.save(fname2, "png")
+    await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
+    if os.path.exists(fname2):
+            os.remove(fname2)
