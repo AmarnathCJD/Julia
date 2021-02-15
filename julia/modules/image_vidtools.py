c7c4e1551b11e987ddb5554e22e52a969d7b6aab
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-15 10:43:41 +0530

    Update image_vidtools.py

--- julia/modules/image_vidtools.py
+++ julia/modules/image_vidtools.py
@@ -155,7 +155,7 @@ async def slogo(event):
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= (image_heightz-h)/2
-    draw.text((x, y), text, font=font, fill="white", stroke_width=30, stroke_fill="black")
+    draw.text((x, y), text, font=font, fill="white", stroke_width=8, stroke_fill="blue")
     fname2 = "LogoBy@FRIDAYOT.png"
     img.save(fname2, "png")
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
