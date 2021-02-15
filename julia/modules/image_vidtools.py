87ca2ce7211de0a78269be4f6c693b7295b96d03
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-15 11:47:55 +0530

    Update image_vidtools.py

--- julia/modules/image_vidtools.py
+++ julia/modules/image_vidtools.py
@@ -195,3 +195,35 @@ async def slogo(event):
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
     if os.path.exists(fname2):
             os.remove(fname2)
+@register(pattern="^/(clogo|cyberlogo) ?(.*)")
+async def slogo(event):
+    if event.sender_id in SUDO_USERS:
+        pass
+    elif event.sender_id == OWNER_ID:
+        pass
+    elif event.sender_id not in SUDO_USERS:
+        await event.reply("Who Are You?")
+        return
+    else:
+        return
+    await event.edit("`Processing..`")
+    text = event.pattern_match.group(2)
+    img = Image.open('./resources/IMG_20210215_104759_504.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "white"
+    shadowcolor = "black"
+    font = ImageFont.truetype("./resources/Vermin Vibes V.otf", 69)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= (image_heightz-h)/2
+    draw.text((x, y), text, font=font, fill="red", stroke_width=9, stroke_fill="green")
+    fname2 = "LogoBy@FRIDAYOT.png"
+    img.save(fname2, "png")
+    await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
+    if os.path.exists(fname2):
+            os.remove(fname2)
