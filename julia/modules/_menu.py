9a65cc0d82b8730718007764b6c288c75b0c4149
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-16 17:57:17 +0530

    Update _menu.py

--- julia/modules/_menu.py
+++ julia/modules/_menu.py
@@ -62,7 +62,6 @@ async def start(event):
         await tbot.send_file(event.chat_id, file=file1)
         await tbot.send_message(
             event.chat_id,
-            file1,
             pm_caption,
             buttons=[
                 [
