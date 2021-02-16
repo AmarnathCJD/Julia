3249a23a1c645e269e8a64862e3a4e24a07e14bc
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-16 13:11:33 +0530

    Update logo.py

--- julia/modules/logo.py
+++ julia/modules/logo.py
@@ -377,14 +377,14 @@ async def slogo(event):
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype("./resources/Road_Rage.otf", 110)
+    font = ImageFont.truetype("./resources/Road_Rage.otf", 290)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, ((image_heightz-h)/2)+350), text, font=font, fill=(255, 255, 255))
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+350)
-    draw.text((x, y), text, font=font, fill="magenta", stroke_width=3, stroke_fill="darkseagreen")
+    y= (image_heightz-h)/2
+    draw.text((x, y), text, font=font, fill="grey", stroke_width=6, stroke_fill="lightgreen")
     fname2 = "LogoBy@FRIDAYOT.png"
     img.save(fname2, "png")
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
