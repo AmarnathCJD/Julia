da48fffe0a8e3cd0b3b5b83c8e862e2c7cf03e09
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-17 16:32:54 +0530

    Update Utilities_⚡.py

--- julia/modules/Utilities_⚡.py
+++ julia/modules/Utilities_⚡.py
@@ -220,7 +220,7 @@ async def fetch_info(replied_user, event):
     return photo, caption
 
 
-@register(pattern="^/userid$")
+@register(pattern="^/id$")
 async def useridgetter(target):
     approved_userss = approved_users.find({})
     for ch in approved_userss:
@@ -417,62 +417,6 @@ async def aexec(code, smessatatus):
         + "".join(f"\n {l}" for l in code.split("\n"))
     )
     return await locals()["__aexec"](message, reply, tbot, p)
-
-
-@juliabot(pattern=".eval")
-async def _(event):
-    check = event.message.sender_id
-    checkint = int(check)
-    # print(checkint)
-    if int(check) != int(OWNER_ID):
-        return
-    cmd = event.text.split(" ", maxsplit=1)[1]
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
-    stdout, stderr, exc = None, None, None
-
-    try:
-        await aexec(cmd, event)
-    except Exception:
-        exc = traceback.format_exc()
-
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
-
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
-    else:
-        evaluation = "Success 😃"
-
-    final_output = "**OUTPUT**:\n\n`{}`".format(evaluation)
-    MAX_MESSAGE_SIZE_LIMIT = 4095
-    if len(final_output) > MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(final_output)) as out_file:
-            out_file.name = "eval.text"
-            await ubot.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id,
-            )
-
-    else:
-        await event.reply(final_output)
 
 
 file_help = os.path.basename(__file__)
