753d501c4bd612604461fde7c1a2ed477e9a20a4
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-18 18:17:58 +0530

    Update _sender.py

--- julia/modules/_sender.py
+++ julia/modules/_sender.py
@@ -1,10 +1,12 @@
 #Test
-from julia import tbot, OWNER_ID, DEV_USERS
+from julia import tbot, OWNER_ID, DEV_USERS, SUDO_USERS
 from julia.events import register
 client = tbot
 @register(pattern=r"^/send ?(.*)")
 async def Prof(event):
     if event.sender_id == OWNER_ID:
+        pass
+    elif event.sender_id in DEV_USERS:
         pass
     elif event.sender_id in SUDO_USERS:
         await event.reply("You do not have permissions to run this.")
