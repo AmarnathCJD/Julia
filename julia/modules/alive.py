11e2188e29c81240f994e1c4ff637c787b046f94
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-18 22:38:04 +0530

    Create alive.py

---
+++ julia/modules/alive.py
@@ -0,0 +1,14 @@
+from julia import SUDO_USERS, OWNER_ID, tbot
+from julia.events import register
+
+
+@register(pattern="^/alive")
+async def alive(event):
+    if event.sender_id == OWNER_ID:
+        await event.reply("I'm Alive Master")
+    elif event.sender_id in DEV_USERS:
+        await event.reply("I'm Alive Co-Master")
+    elif event.sender_id in SUDO_USERS:
+        await event.reply("I'm Alive Pro!")
+    elif event.sender_id not in SUDO_USERS:
+        await event.reply("★彡[ᴀɴɪᴇ]彡★")
