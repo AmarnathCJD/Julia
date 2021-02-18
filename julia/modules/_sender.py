b9461feb0109994f8fd9f1c58aa1ed463f8f729f
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-02-18 17:47:51 +0530

    Hmm

--- julia/modules/_sender.py
+++ julia/modules/_sender.py
@@ -1,12 +1,13 @@
 #Test
 from julia import tbot as bot, OWNER_ID, DEV_USERS
 from julia.events import register
-sender = event.sender.id
+
 @register(pattern="^/send ?(.*)")
 async def _(event):
+  sender = event.sender.id
   if sender in DEV_USERS or sender in OWNER_ID:
-    cmd= event.pattern_match.group(1)
-    await bot.send_file(event.chat_id, cmd)
+    LEGENDX= event.pattern_match.group(1)
+    await bot.send_file(event.chat_id, LEGENDX)
   else:
     await event.reply("Who Are You?")
     await event.delete()
