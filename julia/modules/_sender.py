fdea73d8304170260b35b38a61f8bf8cd97c03d8
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-18 22:03:06 +0530

    Update _sender.py

--- julia/modules/_sender.py
+++ julia/modules/_sender.py
@@ -40,66 +40,11 @@ async def Prof(event):
     else:
         await event.reply("Ekdm Bochlaik😑, File Not Found😆")
 
-papth = path + "thumb_image.jpg"
-@register(pattern="^/rename (.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    thumb = None
-    if os.path.exists(papth):
-        thumb = papth
-    dcevent = await event.reply(
-        "Rename & Upload in process 🙄🙇‍♂️🙇‍♂️🙇‍♀️ It might take some time if file size is big",
-    )
-    input_str = event.pattern_match.group(1)
-    if not os.path.isdir(path):
-        os.makedirs(path)
-    if event.reply_to_msg_id:
-        start = datetime.now()
-        file_name = input_str
-        reply_message = await event.get_reply_message()
-        c_time = time.time()
-        to_download_directory = path
-        downloaded_file_name = os.path.join(to_download_directory, file_name)
-        downloaded_file_name = await event.client.download_media(
-            reply_message,
-            downloaded_file_name,
-            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                progress(d, t, dcevent, c_time, "trying to download", file_name)
-            ),
-        )
-        end = datetime.now()
-        ms_one = (end - start).seconds
-        await event.reply("ok")
-        try:
-            thumb = await reply_message.download_media(thumb=-1)
-        except Exception:
-            thumb = thumb
-        if os.path.exists(downloaded_file_name):
-            c_time = time.time()
-            hmm = await event.client.send_file(
-                event.chat_id,
-                downloaded_file_name,
-                force_document=False,
-                supports_streaming=True,
-                allow_cache=False,
-                reply_to=event.message.id,
-                thumb=thumb,
-                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                    progress(
-                        d, t, event, c_time, "trying to upload", downloaded_file_name
-                    )
-                ),
-            )
-            end_two = datetime.now()
-            os.remove(downloaded_file_name)
-            ms_two = (end_two - end).seconds
-            await dcevent.edit(
-                f"Downloaded file in {ms_one} seconds.\nUploaded in {ms_two} seconds."
-            )
-            await asyncio.sleep(2)
-            await dcevent.delete()
-        else:
-            await dcevent.edit("File Not Found {}".format(input_str))
-    else:
-        await dcevent.edit(".rename file.name as reply to a Telegram media/file")
+@register(pattern="^/ttf ?(.*)")
+async def get(event):
+    name = event.text[5:]
+    m = await event.get_reply_message()
+    with open(name, "w") as f:
+        f.write(m.message)
+    await event.delete()
+    await tbot.send_file(event.chat_id, name, force_document=True)
