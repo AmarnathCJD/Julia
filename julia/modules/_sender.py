21881f877b7e0575084f2be1af2edd7e737bd580
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-18 18:15:19 +0530

    Update _sender.py

--- julia/modules/_sender.py
+++ julia/modules/_sender.py
@@ -4,9 +4,15 @@ from julia.events import register
 client = tbot
 @register(pattern=r"^/send ?(.*)")
 async def Prof(event):
+    if event.sender_id == OWNER_ID:
+        pass
+    elif event.sender_id in SUDO_USERS:
+        await event.reply("You do not have permissions to run this.")
+        return
+    else:
+        return
     input_str = event.pattern_match.group(1)
     the_plugin_file = "./julia/modules/{}.py".format(input_str)
-    await event.reply("test")
     message_id = event.message.id
     await event.client.send_file(
             event.chat_id,
