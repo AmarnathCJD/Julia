94c8e65549a02f14d90ea7bda6f723d05748b3b0
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-18 17:49:10 +0530

    Update _sender.py

--- julia/modules/_sender.py
+++ julia/modules/_sender.py
@@ -4,8 +4,8 @@ from julia.events import register
 
 @register(pattern="^/send ?(.*)")
 async def _(event):
-  sender = event.sender.id
-  if sender in DEV_USERS or sender in OWNER_ID:
+  person = event.sender.id
+  if person in DEV_USERS or person in OWNER_ID:
     LEGENDX= event.pattern_match.group(1)
     await bot.send_file(event.chat_id, LEGENDX)
   else:
