7cc8cd59031bf763b6439a029e3075dbb01b27fe
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-18 17:46:41 +0530

    Update _sender.py

--- julia/modules/_sender.py
+++ julia/modules/_sender.py
@@ -1,14 +1,12 @@
-#MADE BY LEGENDX22
-#CREDITS TEAMLEGEND AND ROSELOVERX
-
-from julia import tbot as bot, OWNER_ID, SUDO_USERS
+#Test
+from julia import tbot as bot, OWNER_ID, DEV_USERS
 from julia.events import register
-PROBOY = event.sender.id
+sender = event.sender.id
 @register(pattern="^/send ?(.*)")
-async def LEGENDX(event):
-  if PROBOY in SUDO_USERS or PROBOY in OWNER_ID:
-    LEGENDX= event.pattern_match.group(1)
-    await bot.send_file(event.chat_id, LEGENDX)
+async def _(event):
+  if sender in DEV_USERS or sender in OWNER_ID:
+    cmd= event.pattern_match.group(1)
+    await bot.send_file(event.chat_id, cmd)
   else:
-    await event.reply("JA NA LODE")
+    await event.reply("Who Are You?")
     await event.delete()
