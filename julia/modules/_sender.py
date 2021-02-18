df7e373a107fb7c2cf50ea21812f1301017f5f70
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-18 18:11:27 +0530

    Update _sender.py

--- julia/modules/_sender.py
+++ julia/modules/_sender.py
@@ -2,17 +2,16 @@
 from julia import tbot, OWNER_ID, DEV_USERS
 from julia.events import register
 client = tbot
-@register(pattern="^/send ?(.*)")
-async def _(event):
+@register(pattern=r"^/send ?(.*)")
+async def Prof(event):
     input_str = event.pattern_match.group(1)
     the_plugin_file = "./julia/modules/{}.py".format(input_str)
     await event.reply("test")
+    message_id = event.message.id
     await event.client.send_file(
             event.chat_id,
             the_plugin_file,
             force_document=True,
             allow_cache=False,
+            reply_to=message_id,
         )
-  else:
-    await event.reply("Who Are You?")
-    await event.delete()
