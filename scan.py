6ca9855d3f9bd619eba5d5ca4dd677626c505aec
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-19 14:12:09 +0530

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,12 +1,11 @@
-from pyimagesearch.transform import four_point_transform
+from transform import four_point_transform
 import imutils
 from skimage.filters import threshold_local
 import argparse
 import cv2
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-                help="Path to the image to be scanned")
+ap.add_argument("-i", "--image", required=True, help="Path to the image to be scanned")
 args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
