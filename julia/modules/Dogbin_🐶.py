d6fa0d3ad905485da58ea675756ea142cb1d3b55
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-19 17:52:11 +0530

    Update Dogbin_🐶.py

--- julia/modules/Dogbin_🐶.py
+++ julia/modules/Dogbin_🐶.py
@@ -1,6 +1,6 @@
-from julia import tbot
-from julia.evebts import register 
-
+from julia import tbot as borg
+from julia.events import register 
+from julia import TEMP_DOWNLOAD_DIRECTORY
 import logging
 import os
 from datetime import datetime
@@ -22,6 +22,60 @@ def progress(current, total):
         )
     )
 
+@register(pattern="^/neko ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    datetime.now()
+    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+    input_str = event.pattern_match.group(1)
+    message = "SYNTAX: `.neko <long text to include>`"
+    if input_str:
+        message = input_str
+    elif event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.media:
+            downloaded_file_name = await borg.download_media(
+                previous_message,
+                TEMP_DOWNLOAD_DIRECTORY,
+                progress_callback=progress,
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            message = ""
+            for m in m_list:
+                # message += m.decode("UTF-8") + "\r\n"
+                message += m.decode("UTF-8")
+            os.remove(downloaded_file_name)
+        else:
+            message = previous_message.message
+    else:
+        message = "SYNTAX: `.neko <long text to include>`"
+    if downloaded_file_name.endswith(".py"):
+        py_file = ""
+        py_file += ".py"
+        data = message
+        key = (
+            requests.post("https://nekobin.com/api/documents", json={"content": data})
+            .json()
+            .get("result")
+            .get("key")
+        )
+        url = f"https://nekobin.com/{key}{py_file}"
+    else:
+        data = message
+        key = (
+            requests.post("https://nekobin.com/api/documents", json={"content": data})
+            .json()
+            .get("result")
+            .get("key")
+        )
+        url = f"https://nekobin.com/{key}"
+
+    reply_text = f"Pasted to Nekobin : [neko]({url})"
+    await event.reply(reply_text)
 
 
 
