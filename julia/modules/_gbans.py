eaebbcfa4e5457a8cedaf22cc82ea31197803314
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-19 14:57:18 +0530

    Update _gbans.py

--- julia/modules/_gbans.py
+++ julia/modules/_gbans.py
@@ -63,7 +63,7 @@ async def _(event):
     if r_sender_id in SUDO_USERS:
         await event.reply("Hey that's a sudo user idiot.")
         return
-    k=event.r_sender_id
+    k=event.sender
     fname=k.first_name
     X=k.last_name
     cd = (f"{fname}-{X}") 
