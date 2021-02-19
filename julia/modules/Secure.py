258dec208a31fe8144e5653ff00d8707c5346871
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-19 09:56:51 +0530

    Update Secure.py

--- julia/modules/Secure.py
+++ julia/modules/Secure.py
@@ -8,3 +8,11 @@ async def hmm(event):
     Text = cmd
     k = secureme.encrypt(Text)
     await event.reply(k)
+
+@register(pattern="^/decrypt (.*)")
+async def hmm(event):
+    ok = event.pattern_match.group(1)
+    Text = ok
+    k = secureme.decrypt(Text)
+    await event.reply(k)
+
