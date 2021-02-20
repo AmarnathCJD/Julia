7b73a9cfa11f02e2a20074dfcc54735274bd689e
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-20 14:51:49 +0530

    Update BetaLogo.py

--- julia/modules/BetaLogo.py
+++ julia/modules/BetaLogo.py
@@ -22,12 +22,6 @@ from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 import random
 
-k = ("22", "12", "15", "30", "35", "20", "26")
-st = random.choice(k)
-
-n = ("black", "green", "red", "blue", "magenta", "pink", "lightgreen", "brickred", "cherryred", "brown", "violet", "orange", "yellow", "gold", "silver")
-col = random.choice(n)
-
 @tbot.on(events.NewMessage(pattern="^/logu (.*)"))
 async def slogo(event):
     if event.fwd_from:
@@ -39,21 +33,24 @@ async def slogo(event):
     reason = reasonn.strip()
     await event.reply("`Processing..`")
     text = cid
-    hmm = "IMG_20210219_203337_228.jpg"
-    img = Image.open(f'./resources/{hmm}')
+    hmm = ("IMG_20210219_203337_228.jpg", "IMG_20210210_170521_219.jpg", "IMG_20210215_104759_504.jpg", "IMG_20210215_103846_312.jpg")
+    fun = ("Vermin Vibes V.otf", "Blacksword.otf")
+    lol = random.choice(hmm)
+    test = random.choice(fun)
+    img = Image.open(f'./resources/{lol}')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype("./resources/Blacksword.otf", 125)
+    font = ImageFont.truetype(f"./resources/{test}", 125)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    n = ("black", "green", "red", "blue", "magenta", "pink", "lightgreen", "brickred", "cherryred", "brown", "violet", "orange", "yellow", "gold", "silver")
+    n = ("black", "green", "red", "blue", "black", "lightgreen", "brickred", "black", "violet", "orange", "yellow", "gold", "silver")
     col = random.choice(n)
     draw.text((x, y), text, font=font, fill=f"{reason}", stroke_width=8, stroke_fill=f"{col}")
     fname2 = "LogoBy@FRIDAYOT.png"
