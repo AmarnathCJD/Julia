115072d205d42dbee69b88318ad3a005fc1a420f
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-20 12:07:50 +0530

    Update truth_dare.py

--- julia/modules/truth_dare.py
+++ julia/modules/truth_dare.py
@@ -1,72 +1,16 @@
 from random import randint
-from PIL import ImageEnhance, ImageOps
 from random import uniform
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-import asyncio
 from julia import OWNER_ID
 from julia import SUDO_USERS
-import io
-import os
 import random
-import re
-import string
-import nltk
-import bs4
-import requests
-from PIL import Image
-from zalgo_text import zalgo
 from julia.events import register
-import json
-import subprocess
-import textwrap
-import urllib.request
 from random import randrange
-from typing import List
-from typing import Optional
-import emoji
-from cowpy import cow
-from fontTools.ttLib import TTFont
-from PIL import ImageDraw
-from PIL import ImageFont
-from pymongo import MongoClient
 from telethon import *
 from telethon.tl import functions
 from telethon.tl.types import *
 from julia import *
-nltk.download("punkt")
-nltk.download("averaged_perceptron_tagger")
 import time
 from datetime import datetime
-WIDE_MAP = {i: i + 0xFEE0 for i in range(0x21, 0x7F)}
-WIDE_MAP[0x20] = 0x3000
-
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
-db = client["missjuliarobot"]
-approved_users = db.approve
-
-
-async def is_register_admin(chat, user):
-    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-
-        return isinstance(
-            (await
-             tbot(functions.channels.GetParticipantRequest(chat,
-                                                           user))).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
-        )
-    if isinstance(chat, types.InputPeerChat):
-
-        ui = await tbot.get_peer_id(user)
-        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id)
-                         )).full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
-        )
-    return None
 
 
 TRUTH = (
@@ -143,38 +87,17 @@ DARE = (
     "Set your crush’s picture as your dp.",
     "Suggest me more dares.",
 )
-GAND = (
-    "GAND successfully Blocked🤔🤔b",
-)
 
 @register(pattern="^/truth$")
 async def msg(event):
-    approved_userss = approved_users.find({})
-    for ch in approved_userss:
-        iid = ch["id"]
-        userss = ch["user"]
-    if event.is_group:
-        if (await is_register_admin(event.input_chat, event.message.sender_id)):
-            pass
-        elif event.chat_id == iid and event.sender_id == userss:
-            pass
-        else:
-            return
+    if event.fwd_from:
+        return
     await event.reply(random.choice(TRUTH))
 
 @register(pattern="^/dare$")
 async def msg(event):
-    approved_userss = approved_users.find({})
-    for ch in approved_userss:
-        iid = ch["id"]
-        userss = ch["user"]
-    if event.is_group:
-        if (await is_register_admin(event.input_chat, event.message.sender_id)):
-            pass
-        elif event.chat_id == iid and event.sender_id == userss:
-            pass
-        else:
-            return
+    if event.fwd_from:
+        return
     await event.reply(random.choice(DARE))
 CC = (
     "#LIVE ◈ 5124874660020502|07|2023|830 ◈ 「Approved (͏CVV) [BlackStone",
@@ -191,7 +114,7 @@ async def msg(event):
     elif event.sender_id == OWNER_ID:
         pass
     elif event.sender_id not in SUDO_USERS:
-        await event.reply("Baag Ja Mewederchod Free Cc only for SuDos😑.")
+        await event.reply("Baag Ja Mewederchod Free CC only for Sudo😑.")
         return
     else:
         return
