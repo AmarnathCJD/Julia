bd1da645313f61ce68587a8b6316383a7afac18b
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-20 22:58:11 +0530

    Update Logo_💥.py

--- julia/modules/Logo_💥.py
+++ julia/modules/Logo_💥.py
@@ -22,7 +22,7 @@ from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 import random
 
-@tbot.on(events.NewMessage(pattern="^/tlogo (.*)"))
+@tbot.on(events.NewMessage(pattern="^/logo (.*)"))
 async def slogo(event):
     if event.sender_id in SUDO_USERS:
         pass
@@ -33,31 +33,21 @@ async def slogo(event):
         return
     else:
         return
-    quew = event.pattern_match.group(1)
-    if "|" in quew:
-        iid, reasonn = quew.split("|")
-    cid = iid.strip()
-    reason = reasonn.strip()
-    await event.reply("`Processing..`")
-    text = cid
-    hmm = ("IMG_20210219_203337_228.jpg", "IMG_20210210_170521_219.jpg", "IMG_20210215_104759_504.jpg", "IMG_20210215_103846_312.jpg")
-    lol = random.choice(hmm)
-    img = Image.open(f'./resources/{lol}')
+    text = event.pattern_match.group(1)
+    img = Image.open('./julia/resources/IMG_20210220_225112_240.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype(f"./resources/Vermin Vibes V.otf", 130)
+    font = ImageFont.truetype(f".julia/resources/fonts/Thicxer-8MerB.ttf", 140)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    n = ("black", "green", "red", "blue", "black", "lightgreen", "black", "violet", "orange", "yellow", "gold", "silver")
-    col = random.choice(n)
-    draw.text((x, y), text, font=font, fill=f"{reason}", stroke_width=8, stroke_fill=f"{col}")
+    draw.text((x, y), text, font=font, fill="red", stroke_width=8, stroke_fill="black")
     fname2 = "LogoBy@FRIDAYOT.png"
     img.save(fname2, "png")
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
