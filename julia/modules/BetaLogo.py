31cd13a36027c8205a941efe6489b019a741c132
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-20 12:53:11 +0530

    Update BetaLogo.py

--- julia/modules/BetaLogo.py
+++ julia/modules/BetaLogo.py
@@ -1,1 +1,53 @@
+from julia.Config import Config
+from julia.events import register
+from julia import CMD_HELP
+from julia import tbot as borg
+from julia import TEMP_DOWNLOAD_DIRECTORY
+import os
+import random
+import numpy as np
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+import pytz 
+import asyncio
+import requests
+from PIL import Image, ImageDraw, ImageFont
+from telegraph import upload_file
+import time
+import html
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
 
+@tbot.on(events.NewMessage(pattern="^/lugo (.*)"))
+async def slogo(event):
+    if event.fwd_from:
+        return
+    quew = event.pattern_match.group(1)
+
+    if "|" in quew:
+        iid, reasonn = quew.split("|")
+    cid = iid.strip()
+    reason = reasonn.strip()
+    await event.edit("`Processing..`")
+    text = cid
+    img = Image.open('./resources/20201125_094030.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "white"
+    shadowcolor = "black"
+    font = ImageFont.truetype("./resources/Blacksword.otf", 600)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill=f"{reason}", stroke_width=30, stroke_fill="black")
+    fname2 = "LogoBy@FRIDAYOT.png"
+    img.save(fname2, "png")
+    await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
+    if os.path.exists(fname2):
+            os.remove(fname2)
