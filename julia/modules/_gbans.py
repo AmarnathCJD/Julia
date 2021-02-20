4bab1697929509adb54432a92c082eb222571e4f
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-20 12:39:08 +0530

    Update _gbans.py

--- julia/modules/_gbans.py
+++ julia/modules/_gbans.py
@@ -44,6 +44,9 @@ async def _(event):
         iid, reasonn = quew.split("|")
     cid = iid.strip()
     reason = reasonn.strip()
+    else:
+        quew = cid
+        reason = ("Unspecified")
     if cid.isnumeric():
         cid = int(cid)
     entity = await tbot.get_input_entity(cid)
