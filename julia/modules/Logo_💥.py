be98ba6920c724490bc540ee0cb28e413b489a53
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-20 23:06:59 +0530

    Update Logo_💥.py

--- julia/modules/Logo_💥.py
+++ julia/modules/Logo_💥.py
@@ -22,32 +22,25 @@ from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 import random
 
-@tbot.on(events.NewMessage(pattern="^/logo (.*)"))
+
+@register(pattern="^/(vl|starlogo) ?(.*)")
 async def slogo(event):
-    if event.sender_id in SUDO_USERS:
-        pass
-    elif event.sender_id == OWNER_ID:
-        pass
-    elif event.sender_id not in SUDO_USERS:
-        await event.reply("Sed")
-        return
-    else:
-        return
-    text = event.pattern_match.group(1)
+    await event.edit("`Processing..`")
+    text = event.pattern_match.group(2)
     img = Image.open('./julia/resources/IMG_20210220_225112_240.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype("./julia/resources/fonts/Thicxer-8MerB.ttf", 140)
+    font = ImageFont.truetype("./julia/resources/fonts/Thicxer-8MerB.ttf", 120)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="red", stroke_width=8, stroke_fill="black")
+    draw.text((x, y), text, font=font, fill="red", stroke_width=30, stroke_fill="black")
     fname2 = "LogoBy@FRIDAYOT.png"
     img.save(fname2, "png")
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
