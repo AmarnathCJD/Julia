8a92b6fac0608a5955002b798a6cd376593aa75d
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-20 12:01:45 +0530

    Update Nekobin_🐶.py

--- julia/modules/Nekobin_🐶.py
+++ julia/modules/Nekobin_🐶.py
@@ -5,10 +5,12 @@ from julia import TEMP_DOWNLOAD_DIRECTORY
 import logging
 import os
 from datetime import datetime
-
+from telethon import events
+import pygments
+from pygments.formatters import ImageFormatter
+from pygments.lexers import Python3Lexer
 import requests
 from requests import exceptions, get
-from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 logging.basicConfig(
@@ -77,12 +79,45 @@ async def _(event):
 
     await event.reply(f"Pasted to Nekobin : [neko]({url})", link_preview=False)
 
+    
+client = borg
+@register(pattern="^/ncode")
+async def coder_print(event):
+    a = await event.client.download_media(
+        await event.get_reply_message(), TEMP_DOWNLOAD_DIRECTORY
+    )
+    s = open(a, "r")
+    c = s.read()
+    s.close()
+    pygments.highlight(
+        f"{c}",
+        Python3Lexer(),
+        ImageFormatter(font_name="DejaVu Sans Mono", line_numbers=True),
+        "result.png",
+    )
+    res = await event.client.send_message(
+        event.chat_id,
+        "Pasting this code on my page...",
+        reply_to=event.reply_to_msg_id,
+    )
+    await event.client.send_file(
+        event.chat_id, "result.png", force_document=True, reply_to=event.reply_to_msg_id
+    )
+    # await event.client.send_file(event.chat_id, "resuly.png",
+    # force_document=False, reply_to=event.reply_to_msg_id)
+    await res.delete()
+    await event.delete()
+    os.remove(a)
+    os.remove("result.png")
+    
+    
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
 
 __help__ = """
- - /neko <text>:Type in reply to a doc or text to paste it to nekobin
+ - /neko: Type in reply to a doc or text to paste it to nekobin
+ - /ncode: Paste Text as Image
 """
 
 CMD_HELP.update({
