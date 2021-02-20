0f0f615403d7481189188d277990d73366c1df5b
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-20 09:56:53 +0530

    Update misc.py

--- julia/modules/misc.py
+++ julia/modules/misc.py
@@ -32,3 +32,64 @@ async def wish_check(event):
         else:
             reslt = f"Make A Wish."
     await event.reply(reslt)
+
+
+
+
+normiefont = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+]
+
+weebyfont = ["Λ", "в", "¢", "∂", "є", "ƒ", "g", "н", "ι", "נ", "к", "ℓ", "м", "η", "σ", "ρ", "q", "я", "ѕ", "т", "υ", "ν", "ω", "χ", "у", "z"]
+
+@register(pattern="^font (.*)")
+async def weebify(event):
+
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await event.reply("`What I am Supposed to Fontify U Dumb`")
+        return
+    string = "".join(args).lower()
+    for normiecharacter in string:
+        if normiecharacter in normiefont:
+            weebycharacter = weebyfont[normiefont.index(normiecharacter)]
+            string = string.replace(normiecharacter, weebycharacter)
+    await event.reply(string)
