90013c28a2bc23260432094138696653e275e916
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-20 11:57:30 +0530

    Update _screenshot.py

--- julia/modules/_screenshot.py
+++ julia/modules/_screenshot.py
@@ -1,73 +1,10 @@
-from random import randint
-#made by RoseLoverX Jo Kang Kiya Wo Mera beta Ho
-from PIL import ImageEnhance, ImageOps
-from random import uniform
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-import asyncio
 import io
-import os
-import random
-import re
-import string
-import nltk
-from PIL import Image
-from zalgo_text import zalgo
-from julia.events import register
-import json
-import subprocess
-import textwrap
-import urllib.request
-from random import randrange
-from typing import List
-from typing import Optional
-import emoji
-from cowpy import cow
-from fontTools.ttLib import TTFont
-from PIL import ImageDraw
-from PIL import ImageFont
-from pymongo import MongoClient
-from telethon import *
-from telethon.tl import functions
-from telethon.tl.types import *
-from julia import *
-from julia.Config import Config
 import traceback
 from datetime import datetime
-nltk.download("punkt")
-nltk.download("averaged_perceptron_tagger")
-
-WIDE_MAP = {i: i + 0xFEE0 for i in range(0x21, 0x7F)}
-WIDE_MAP[0x20] = 0x3000
-
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
-db = client["missjuliarobot"]
-approved_users = db.approve
-
+from selenium import webdriver
+from julia.events import register
 
 GOOGLE_CHROME_BIN = "/app/.apt/usr/bin/google-chrome"
-
-async def is_register_admin(chat, user):
-    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-
-        return isinstance(
-            (await
-             tbot(functions.channels.GetParticipantRequest(chat,
-                                                           user))).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
-        )
-    if isinstance(chat, types.InputPeerChat):
-
-        ui = await tbot.get_peer_id(user)
-        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id)
-                         )).full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
-        )
-    return None
 
 @register(pattern="^/sshot (.*)")
 async def msg(event):
@@ -77,7 +14,7 @@ async def msg(event):
         pass
     else:
         return
-        await event.reply("Painting web-page..")
+    await event.reply("Painting web-page..")
     start = datetime.now()
     try:
         chrome_options = webdriver.ChromeOptions()
