44b915498b0f0e4f88629bb1a2b99ec5c62b6443
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-20 10:03:55 +0530

    Update misc.py

--- julia/modules/misc.py
+++ julia/modules/misc.py
@@ -53,4 +53,5 @@ async def weebify(event):
         if normiecharacter in normiefont:
             weebycharacter = weebyfont[normiefont.index(normiecharacter)]
             string = string.replace(normiecharacter, weebycharacter)
+    await event.reply("hmm")
     await event.reply(string)
