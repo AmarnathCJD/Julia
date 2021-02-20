df4d09f6ce46a2c36658b9ecf2d7c756c1cf8e15
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-20 14:32:14 +0530

    Update BetaLogo.py

--- julia/modules/BetaLogo.py
+++ julia/modules/BetaLogo.py
@@ -20,33 +20,39 @@ from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
+import random
+
+k = ("22", "12", "15", "30", "35", "20", "26")
+st = random.choice(k)
+
+n = ("black", "green", "red", "blue", "magenta", "pink", "lightgreen", "brickred", "cherryred", "brown", "violet", "orange", "yellow", "gold", "silver")
+col = random.choice(n)
 
 @tbot.on(events.NewMessage(pattern="^/lugo (.*)"))
 async def slogo(event):
     if event.fwd_from:
         return
     quew = event.pattern_match.group(1)
-
     if "|" in quew:
         iid, reasonn = quew.split("|")
     cid = iid.strip()
     reason = reasonn.strip()
-    await event.edit("`Processing..`")
+    await event.reply("`Processing..`")
     text = cid
-    img = Image.open('./resources/20201125_094030.jpg')
+    img = Image.open('./resources/IMG_20210219_203337_228.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype("./resources/Blacksword.otf", 600)
+    font = ImageFont.truetype("./resources/Blacksword.otf", 125)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill=f"{reason}", stroke_width=30, stroke_fill="black")
+    draw.text((x, y), text, font=font, fill=f"{reason}", stroke_width=f"{st}", stroke_fill=f"{col}")
     fname2 = "LogoBy@FRIDAYOT.png"
     img.save(fname2, "png")
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
