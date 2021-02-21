c5975666bfc6a318268e302f0ea20221e860d1ed
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-21 10:57:05 +0530

    Update Logo_💥.py

--- julia/modules/Logo_💥.py
+++ julia/modules/Logo_💥.py
@@ -24,7 +24,7 @@ from telethon.utils import get_input_location
 import random
 
 
-@register(pattern="^/(l|starlogo) ?(.*)")
+@register(pattern="^/(1|starlogo) ?(.*)")
 async def slogo(event):
     await event.reply("`Processing..`")
     text = event.pattern_match.group(2)
@@ -34,14 +34,153 @@ async def slogo(event):
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype("./julia/resources/UMBRAN.TTF", 300)
+    font = ImageFont.truetype("./julia/resources/AVENGEANCE HEROIC AVENGER BD.otf", 300)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="yellow", stroke_width=15, stroke_fill="black")
+    draw.text((x, y), text, font=font, fill="black", stroke_width=15, stroke_fill="yellow")
+    fname2 = "LogoBy@FRIDAYOT.png"
+    img.save(fname2, "png")
+    await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+
+@register(pattern="^/(2|starlogo) ?(.*)")
+async def slogo(event):
+    await event.reply("`Processing..`")
+    text = event.pattern_match.group(2)
+    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "white"
+    shadowcolor = "black"
+    font = ImageFont.truetype("./julia/resources/Zilap Monograma DEMO.ttf", 300)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="orange", stroke_width=15, stroke_fill="black")
+    fname2 = "LogoBy@FRIDAYOT.png"
+    img.save(fname2, "png")
+    await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+@register(pattern="^/(3|starlogo) ?(.*)")
+async def slogo(event):
+    await event.reply("`Processing..`")
+    text = event.pattern_match.group(2)
+    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "white"
+    shadowcolor = "black"
+    font = ImageFont.truetype("./julia/resources/PRINCETN.TTF", 300)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="blue", stroke_width=15, stroke_fill="black")
+    fname2 = "LogoBy@FRIDAYOT.png"
+    img.save(fname2, "png")
+    await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+@register(pattern="^/(4|starlogo) ?(.*)")
+async def slogo(event):
+    await event.reply("`Processing..`")
+    text = event.pattern_match.group(2)
+    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "white"
+    shadowcolor = "black"
+    font = ImageFont.truetype("./julia/resources/HARLOWN.TTF", 300)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="pink", stroke_width=15, stroke_fill="blue")
+    fname2 = "LogoBy@FRIDAYOT.png"
+    img.save(fname2, "png")
+    await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+@register(pattern="^/(5|starlogo) ?(.*)")
+async def slogo(event):
+    await event.reply("`Processing..`")
+    text = event.pattern_match.group(2)
+    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "white"
+    shadowcolor = "black"
+    font = ImageFont.truetype("./julia/resources/Vampire Calligraphy - TTF.ttf", 300)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="yellow", stroke_width=15, stroke_fill="blue")
+    fname2 = "LogoBy@FRIDAYOT.png"
+    img.save(fname2, "png")
+    await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+@register(pattern="^/(6|starlogo) ?(.*)")
+async def slogo(event):
+    await event.reply("`Processing..`")
+    text = event.pattern_match.group(2)
+    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "white"
+    shadowcolor = "black"
+    font = ImageFont.truetype("./julia/resources/wolfsbane2ii.ttf", 300)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="green", stroke_width=15, stroke_fill="grey")
+    fname2 = "LogoBy@FRIDAYOT.png"
+    img.save(fname2, "png")
+    await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+@register(pattern="^/(7|starlogo) ?(.*)")
+async def slogo(event):
+    await event.reply("`Processing..`")
+    text = event.pattern_match.group(2)
+    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "white"
+    shadowcolor = "black"
+    font = ImageFont.truetype("./julia/resources/Vampire Wars.otf", 300)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="red", stroke_width=15, stroke_fill="grey")
     fname2 = "LogoBy@FRIDAYOT.png"
     img.save(fname2, "png")
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
