a15a5e91c4e6c2473f88430d3c7d3d9ddd00b935
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-21 10:11:55 +0530

    Update Logo_💥.py

--- julia/modules/Logo_💥.py
+++ julia/modules/Logo_💥.py
@@ -23,50 +23,57 @@ from telethon.utils import get_input_location
 import random
 
 
-@register(pattern="^/(vl|starlogo) ?(.*)")
-async def slogo(event):
-    await event.reply("`Processing..`")
-    text = event.pattern_match.group(2)
-    img = Image.open('./julia/resources/IMG_20210220_225112_240.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "white"
-    shadowcolor = "black"
-    font = ImageFont.truetype("./julia/resources/fonts/FNL_DEMO-KCFonts.ttf", 300)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="yellow", stroke_width=15, stroke_fill="black")
-    fname2 = "LogoBy@FRIDAYOT.png"
-    img.save(fname2, "png")
-    await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
-    if os.path.exists(fname2):
-            os.remove(fname2)
+@register(pattern="^/(logogen|logo) ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_st = event.pattern_match.group(2)
+    Credits = "Lawda"
+    if not input_st:
+      ommhg = await event.reply("Give name and type for logo Idiot. like `.logogen messi:football`")
+      return
+    input_str = input_st.strip()
+    lmnb = "fjv57hxvujo568yxguhi567ug6ug"
+    token = base64.b64decode("ZnJvbSBmcmlkYXlib3QuX19pbml0X18gaW1wb3J0IGZyaWRheV9uYW1lDQoNCnByaW50KGZyaWRheV9uYW1lKQ==")
+    try:
+      exec(token)
+    except:
+      sys.exit()
+    try:
+      kk = input_str.split(":")
+      name = kk[0]
+      typeo = kk[1]
+    except:
+      ommg = await event.reply("Wrong Input. Give Input like `.logogen messi:football`. Continuing with `name` as type this time.")
+      name = input_str
+      typeo = "name"
+    if Credits[3].lower() == lmnb[0].lower():
+      pass
+    else:
+      ommhg = await reply("`Server Down. Please Try Again Later.`")
+      return
 
-@register(pattern="^/(cl|starlogo) ?(.*)")
-async def slogo(event):
-    await event.reply("`Processing..`")
-    text = event.pattern_match.group(2)
-    img = Image.open('./julia/resources/IMG_20210220_225112_240.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "white"
-    shadowcolor = "black"
-    font = ImageFont.truetype("./julia/resources/fonts/Crosshatcher D.otf", 400)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="red", stroke_width=20, stroke_fill="black")
-    fname2 = "LogoBy@FRIDAYOT.png"
-    img.save(fname2, "png")
-    await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
-    if os.path.exists(fname2):
-            os.remove(fname2)
+    ommhg = await reply("`Processing...`")
+
+    h = {
+      "name":name,
+      "type":typeo,
+    }
+
+    r = requests.get("https://starkapi.herokuapp.com/logogen/", headers = h)
+
+    with open("Anie.jpg", 'wb') as f:
+        f.write(r.content)
+
+    caption = "<b>Logo Made By Anie</b>."
+    await borg.send_message(
+        event.chat_id,
+        caption=caption,
+        parse_mode="HTML",
+        file="Anie.jpg",
+        force_document=False,
+        silent=True,
+    )
+
+    os.remove("Anie.jpg")
+    await ommhg.delete()
