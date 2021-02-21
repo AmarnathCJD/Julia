87a9260880d857d0cd409015970d2f259e0d715a
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-21 18:02:40 +0530

    Update _screenshot.py

--- julia/modules/_screenshot.py
+++ julia/modules/_screenshot.py
@@ -16,7 +16,7 @@ async def _(event):
     input_str = event.pattern_match.group(1)
     response_api = requests.get(
         sample_url.format(
-            SCREEN_SHOT_LAYER_ACCESS_KEY, input_str, "1", "2560x1440", "PNG", "1"
+            SCREEN_SHOT_LAYER_ACCESS_KEY, input_str, "1", "2220x1080", "PNG", "1"
         )
     )
     # https://stackoverflow.com/a/23718458/4723940
