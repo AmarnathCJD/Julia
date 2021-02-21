38c50d07b030284d6bdd35055fedfdc4151c17a8
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-21 12:00:29 +0530

    Update Logo_💥.py

--- julia/modules/Logo_💥.py
+++ julia/modules/Logo_💥.py
@@ -48,17 +48,17 @@ async def slogo(event):
     if os.path.exists(fname2):
             os.remove(fname2)
 
-@register(pattern="^/(logo|starlogo) ?(.*)")
+@register(pattern="^/(slogo|starlogo) ?(.*)")
 async def slogo(event):
     await event.reply("`Processing..`")
     text = event.pattern_match.group(2)
-    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    img = Image.open('./julia/resources/20201125_094030.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype("./julia/resources/Zilap Monograma DEMO.ttf", 300)
+    font = ImageFont.truetype("./julia/resources/Blacksword.otf", 300)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -75,20 +75,20 @@ async def slogo(event):
 async def slogo(event):
     await event.reply("`Processing..`")
     text = event.pattern_match.group(2)
-    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    img = Image.open('./julia/resources/IMG_20210210_170521_219.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype("./julia/resources/PRINCETN.TTF", 300)
+    font = ImageFont.truetype("./julia/resources/PRINCETN.TTF", 500)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="blue", stroke_width=15, stroke_fill="black")
+    draw.text((x, y), text, font=font, fill="white", stroke_width=18, stroke_fill="black")
     fname2 = "LogoBy@FRIDAYOT.png"
     img.save(fname2, "png")
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
@@ -98,20 +98,20 @@ async def slogo(event):
 async def slogo(event):
     await event.reply("`Processing..`")
     text = event.pattern_match.group(2)
-    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    img = Image.open('./julia/resources/Explosion (7).png')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype("./julia/resources/HARLOWN.TTF", 300)
+    font = ImageFont.truetype("./julia/resources/Vermin Vibes V.otf", 300)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="pink", stroke_width=15, stroke_fill="blue")
+    draw.text((x, y), text, font=font, fill="blue", stroke_width=15, stroke_fill="white")
     fname2 = "LogoBy@FRIDAYOT.png"
     img.save(fname2, "png")
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
@@ -121,20 +121,20 @@ async def slogo(event):
 async def slogo(event):
     await event.reply("`Processing..`")
     text = event.pattern_match.group(2)
-    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    img = Image.open('./julia/resources/Explosion (5).png')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype("./julia/resources/Vampire Calligraphy - TTF.ttf", 300)
+    font = ImageFont.truetype("./julia/resources/Vermin Vibes V.otf", 300)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="yellow", stroke_width=15, stroke_fill="blue")
+    draw.text((x, y), text, font=font, fill="yellow", stroke_width=15, stroke_fill="black")
     fname2 = "LogoBy@FRIDAYOT.png"
     img.save(fname2, "png")
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
@@ -144,20 +144,20 @@ async def slogo(event):
 async def slogo(event):
     await event.reply("`Processing..`")
     text = event.pattern_match.group(2)
-    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    img = Image.open('./julia/resources/IMG_20210215_104759_504.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype("./julia/resources/wolfsbane2ii.ttf", 300)
+    font = ImageFont.truetype("./julia/resources/Vermin Vibes V.otf", 300)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="green", stroke_width=15, stroke_fill="grey")
+    draw.text((x, y), text, font=font, fill="white", stroke_width=15, stroke_fill="grey")
     fname2 = "LogoBy@FRIDAYOT.png"
     img.save(fname2, "png")
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
@@ -167,20 +167,20 @@ async def slogo(event):
 async def slogo(event):
     await event.reply("`Processing..`")
     text = event.pattern_match.group(2)
-    img = Image.open('./julia/resources/Blankmeisnub.jpg')
+    img = Image.open('./julia/resources/IMG_20210216_101425_014.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "black"
-    font = ImageFont.truetype("./julia/resources/Vampire Wars.otf", 300)
+    font = ImageFont.truetype("./julia/resources/Vampire Wars.otf", 200)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="red", stroke_width=15, stroke_fill="grey")
+    draw.text((x, y), text, font=font, fill="black", stroke_width=15, stroke_fill="grey")
     fname2 = "LogoBy@FRIDAYOT.png"
     img.save(fname2, "png")
     await borg.send_file(event.chat_id, fname2, caption="Made By Anie")
