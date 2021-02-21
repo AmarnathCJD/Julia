7f2a3aae65f1ec3c2463a786aa8ef7c57bb1f7d1
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-21 13:23:20 +0530

    Update _gbans.py

--- julia/modules/_gbans.py
+++ julia/modules/_gbans.py
@@ -3,7 +3,7 @@ from telethon.tl.types import ChatBannedRights
 from telethon import events
 from telethon.tl.functions.channels import EditBannedRequest
 from pymongo import MongoClient
-from julia import MONGO_DB_URI, GBAN_LOGS
+from julia import MONGO_DB_URI
 
 BANNED_RIGHTS = ChatBannedRights(
     until_date=None,
