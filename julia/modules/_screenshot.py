1c623f2299ca40ce9b84f3eb9bdbc837870eb96b
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-21 17:59:17 +0530

    Update _screenshot.py

--- julia/modules/_screenshot.py
+++ julia/modules/_screenshot.py
@@ -1,76 +1,39 @@
+"""Take screenshot of any website
+Syntax: .screencapture <Website URL>"""
+
 import io
-import os
-import traceback
-from julia import CMD_HELP
-from selenium import webdriver
+from julia import tbot as borg
+import requests
 from julia.events import register
-from selenium.webdriver.chrome.options import Options
-from julia.Config import Config
-from julia import tbot
-GOOGLE_CHROME_BIN = "/home/ubuntu/"
 
+SCREEN_SHOT_LAYER_ACCESS_KEY = "ed5c9f4f66729019d4a0ec6bdcabd5bd"
 @register(pattern="^/sshot (.*)")
-async def msg(event):
-    if event.sender_id == OWNER_ID:
-        pass
-    else:
+async def _(event):
+    if event.fwd_from:
         return
-    try:
-        k = await event.reply("Painting Web_Page...")
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--ignore-certificate-errors")
-        chrome_options.add_argument("--test-type")
-        chrome_options.add_argument("--headless")
-        # https://stackoverflow.com/a/53073789/4723940
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.binary_location=GOOGLE_CHROME_BIN
-        driver = webdriver.Chrome(chrome_options=chrome_options)
-        input_str = event.pattern_match.group(1)
-        imp = "anie"
-        driver.get(input_str)
-        height = driver.execute_script(
-            "return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);"
+    await event.reply("Painting Web_page...")
+    sample_url = "https://api.screenshotlayer.com/api/capture?access_key={}&url={}&fullpage={}&viewport={}&format={}&force={}"
+    input_str = event.pattern_match.group(1)
+    response_api = requests.get(
+        sample_url.format(
+            SCREEN_SHOT_LAYER_ACCESS_KEY, input_str, "1", "2560x1440", "PNG", "1"
         )
-        width = driver.execute_script(
-            "return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);"
-        )
-        driver.set_window_size(width + 100, height + 100)
-        # Add some pixels on top of the calculated dimensions
-        # for good measure to make the scroll bars disappear
-        im_png = driver.get_screenshot_as_png()
-        # saves screenshot of entire page
-        driver.close()
-        message_id = event.message.id
-        if event.reply_to_msg_id:
-            message_id = event.reply_to_msg_id
-        with io.BytesIO(im_png) as out_file:
-            out_file.name = "Anie.sshot.PNG"
-            await tbot.send_file(
-                event.chat_id,
-                out_file,
-                caption=imp,
-                force_document=True,
-                reply_to=message_id,
-                allow_cache=False,
-                silent=True,
-            )
-        await k.delete()
-    except Exception:
-        await k.delete()
-        await event.reply("Not a Valid Url")
-
-file_help = os.path.basename(__file__)
-file_help = file_help.replace(".py", "")
-file_helpo = file_help.replace("_", " ")
-
-__help__ = """
- - /sshot: Get Screenshot Of a Website
-"""
-
-CMD_HELP.update({
-    file_helpo: [
-        file_helpo,
-        __help__
-    ]
-})
+    )
+    # https://stackoverflow.com/a/23718458/4723940
+    contentType = response_api.headers["content-type"]
+    if "image" in contentType:
+        with io.BytesIO(response_api.content) as screenshot_image:
+            screenshot_image.name = "Anie.png"
+            try:
+                await borg.send_file(
+                    event.chat_id,
+                    screenshot_image,
+                    caption=input_str,
+                    force_document=True,
+                    reply_to=event.message.reply_to_msg_id,
+                )
+                await event.delete()
+            except Exception as e:
+                await event.reply(str(e))
+    else:
+        await event.reply(response_api.text)
