b3c43f087386d123ab1a5814cf8688c7e37e6eb6
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-22 17:32:22 +0530

    Update __init__.py

--- julia/__init__.py
+++ julia/__init__.py
@@ -14,7 +14,7 @@ StartTime = time.time()
 CMD_LIST = {}
 CMD_HELP = {}
 LOAD_PLUG = {}
-BOT_VERSION = "1.1.1"
+BOT_VERSION = "1.1.2"
 
 # enable logging
 logging.basicConfig(
@@ -25,35 +25,36 @@ LOGGER = logging.getLogger(__name__)
 ENV = bool(os.environ.get("ENV", True))
 
 if ENV:
-    TOKEN = "1484701846:AAHuhlkYx5IuW5BSc0fdHhQMFyOh8f_5JAQ"
-    OWNER_ID = 1221693726
-    OWNER_USERNAME = "RoseLoverX"
-    API_KEY = 1822414
-    API_HASH = "46f1888d3f68396bad08c92ac4d7f00a"
-    DB_URI = "postgres://okebzhizjpyfrf:792896d97d959a7fd4b1cb91c66bb621ec075aa83d8278728a33b77f8c3672a7@ec2-52-50-171-4.eu-west-1.compute.amazonaws.com:5432/d23aaaj6ddar4f"
-    SUDO_USERS = 1383009042
-    DEV_USERS = None
-    UPSTREAM_REPO_URL = "https://github.com/amarnathcdj/julia"
-    MONGO_DB_URI = "mongodb+srv://newgay:nub123@cluster0.hlrtz.mongodb.net/anie?retryWrites=true&w=majority"
-    REM_BG_API_KEY = None
-    TIME_API_KEY = None
-    IBM_WATSON_CRED_URL = None
-    IBM_WATSON_CRED_PASSWORD = None
-    HEROKU_APP_NAME = None
-    TEMPORARY_DATA = None
-    CONSOLE_LOGGER_VERBOSE = False
-    BOT_ID = 1484701846
-    CHROME_DRIVER = "/app/.chromedriver/bin/chromedriver"
-    GOOGLE_CHROME_BIN = "/app/.apt/usr/bin/google-chrome"
+    TOKEN = os.environ.get("TOKEN", None)
+    OWNER_ID = int(os.environ.get("OWNER_ID", None))
+    GBAN_LOGS = os.environ.get("GBAN_LOGS", None)
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
+    SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "").split()}
+    API_KEY = os.environ.get("API_KEY", None)
+    API_HASH = os.environ.get("API_HASH", None)
+    OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
+    DB_URI = os.environ.get("DATABASE_URL")
+    YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    WOLFRAM_ID = os.environ.get("WOLFRAM_ID", None)
+    LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
     tbot = TelegramClient(None, API_KEY, API_HASH)
-    LYDIA_API_KEY = None
-    WOLFRAM_ID = "V4GHVG-AKHHG599KP"
-    TEMP_DOWNLOAD_DIRECTORY = "./"
-    WALL_API = "e87add43be4c43a58c00ec5957ab250b"
-    YOUTUBE_API_KEY = None
-    VIRUS_API_KEY = None
-    CASH_API_KEY = None
-    OPENWEATHERMAP_ID = "dbab2b41c8871bb3ef0627d9ae4e4693"
+    SUDO_USERS = list(SUDO_USERS)
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+    IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
+    IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
+    WALL_API = os.environ.get("WALL_API", None)
+    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
+    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
+    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
+    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
+    VIRUS_API_KEY = os.environ.get("VIRUS_API_KEY", None)
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    TEMPORARY_DATA = os.environ.get("TEMPORARY_DATA", None)
+    UPSTREAM_REPO_URL = os.environ.get("UPSTREAM_REPO_URL", None)
+    CONSOLE_LOGGER_VERBOSE = os.environ.get("CONSOLE_LOGGER_VERBOSE", "False")
+    BOT_ID = int(os.environ.get("BOT_ID", None))
     if CONSOLE_LOGGER_VERBOSE:
         basicConfig(
             format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=DEBUG
@@ -63,7 +64,7 @@ if ENV:
             format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=INFO
         )
     LOGS = getLogger(__name__)
-    STRING_SESSION = "1BVtsOIgBuxbp6HU1bRBeYEdSmNVKcuTjR8C5chKx2BqzyaIRTImT0Qj6YM-p-st0m_wTVORWMvRm1mT8CUcqRLhD52wfGhW203eoqsMrnDPMunoY7dkMjfFa80u-SFw8bMV8BA-rYHOyv1NhrPJijqkXGIqX3NSUUtYIE3lbM52r3Cavc17XR-k6OkTayCx8N2WaWjS_egO8XKJOr-jVkf_piwabAf2zLNXLea3IPZ5HF7IWy-YtJ1tIjdnh98biiMldh7wLTfwFyI-Ui0Rqfqz6nrFJazftue5QY-yHsNj9L9T2UhqsEHKlaV2Ox-NiLDvBmnBRUrnEKi3L_0EXfehIQypxvuw="
+
     if STRING_SESSION:
         ubot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
     else:
@@ -72,5 +73,8 @@ if ENV:
     try:
         ubot.start()
     except BaseException:
-        print("Sed Bra")
+        print("Network Error !")
         sys.exit(1)
+
+else:
+    sys.exit(1)
