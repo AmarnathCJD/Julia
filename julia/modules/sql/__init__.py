2f495159ac0cd32dc7240ee2c795643a6005325b
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-25 14:58:36 +0530

    Update __init__.py

--- julia/modules/sql/__init__.py
+++ julia/modules/sql/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
-from julia import DB_URI
+DB_URI = "postgres://uemcxpmiublroc:05a41eae1de2eef1d72c68b260d7a0ce3b2155636ebef0bd648c713d2ae86012@ec2-52-70-67-123.compute-1.amazonaws.com:5432/d1qsmgemcfcvj7"
 
 
 def start() -> scoped_session:
